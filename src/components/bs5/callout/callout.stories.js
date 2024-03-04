e11ce051ec3fc9ef3e92e52e722ac6c8203901c1
Jordan Quigg <jordan.quigg@chde.qld.gov.au>
2024-03-04 16:29:10 +1000

    QOLOE-14 Callout initial build

--- src/components/bs5/callout/callout.stories.js
+++ src/components/bs5/callout/callout.stories.js
@@ -1,4 +1,4 @@
-// Blockquote.stories.js
+// callout.stories.js
 import { Callout } from './Callout.js';
 import defaultdata from './callout.data.json';
 
@@ -6,17 +6,46 @@ export default {
   tags: ['autodocs'],
   title: 'Components/Callout',
   render: (args) => new Callout(args).html,
-  
+
   argTypes: {
-    // breadcrumbs: {
-    //   name: 'Breadcrumbs',
-    //   control: 'object',
-    //   description: `Pass a JSON object containing an array of links to the component.`,
-    // },
+    title: {
+      name: 'Title',
+      description: `H3 title text for the callout.`,
+    },
+    content: {
+      name: 'Content',
+      description: 'Text content for the callout. Can include HTML markup.',
+    },
+  },
+
+  /**
+   * Additional parameters for the story.
+   *
+   * @type {Object}
+   * @property {Object} design - Configuration for the design parameter.
+   * @property {string} design.name - Name of the design parameter.
+   * @property {string} design.type - Type of the design parameter.
+   * @property {string} design.url - URL of the design parameter.
+   */
+  parameters: {
+    design: {
+      name: "QGDS Figma Reference",
+      type: "figma",
+      url: "https://www.figma.com/file/qKsxl3ogIlBp7dafgxXuCA/QLD-GOV-DDS?type=design&node-id=5990-98115&mode=design&t=Ue7c77KjVYU1eTGj-0",
+    },
   },
 };
 
-// Default Breadcrumbs story
+/**
+ * Default Callout
+ */
 export const Default = {
-  args: defaultdata,
+  args: defaultdata.default,
+};
+
+/**
+ * Callout without title
+ */
+export const NoTitle = {
+  args: defaultdata.noTitle,
 };
