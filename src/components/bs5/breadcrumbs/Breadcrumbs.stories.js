05ded26b4a0cd92984f0b7c5ebfb46528dc59bee
Jordan Quigg <jordan.quigg@chde.qld.gov.au>
2024-03-08 14:21:37 +1000

    QOLOE-12 update stories

--- src/components/bs5/breadcrumbs/Breadcrumbs.stories.js
+++ src/components/bs5/breadcrumbs/Breadcrumbs.stories.js
@@ -6,7 +6,7 @@ export default {
   tags: ['autodocs'],
   title: 'Components/Breadcrumbs',
   render: (args) => new Breadcrumbs(args).html,
-  
+
   argTypes: {
     breadcrumbs: {
       name: 'Breadcrumbs',
@@ -14,9 +14,36 @@ export default {
       description: `Pass a JSON object containing an array of links to the component.`,
     },
   },
+
+
+  /**
+   * Additional parameters for the story.
+   *
+   * @type {Object}
+   * @property {Object} design - Configuration for the design parameter.
+   * @property {string} design.name - Name of the design parameter.
+   * @property {string} design.type - Type of the design parameter.
+   * @property {string} design.url - URL of the design parameter.
+   */
+  parameters: {
+    design: {
+      name: "QGDS Figma Reference",
+      type: "figma",
+      url: "https://www.figma.com/file/qKsxl3ogIlBp7dafgxXuCA/QLD-GOV-DDS?type=design&node-id=5990-98076&mode=design&t=Ge7frKNP4uEGswUz-0",
+    },
+  },
 };
 
-// Default Breadcrumbs story
+/**
+ * Default Breadcrumbs story
+ * */
 export const Default = {
-  args: defaultdata,
+  args: defaultdata.default,
+};
+
+/**
+ * Default Breadcrumbs story
+ * */
+export const forGov = {
+  args: defaultdata.forGov,
 };
