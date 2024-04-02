67b05002ee8cf1b02afbf0bacb33db2194740fa3
Mitchell Anderson <mitchellanderson@Mitchells-MacBook-Pro.local>
2024-04-02 16:21:34 +1000

    Updated to not include megamenu as seperate component

--- src/components/bs5/navbar/navbar.stories.js
+++ src/components/bs5/navbar/navbar.stories.js
@@ -2,10 +2,6 @@
 import { Navbar } from './Navbar.js';
 import { default_state, menu_state} from './navbar.data.json';
 
-// megaMenu (dropdown) component
-import { megaMenu } from '../megaMenu/megaMenu.js';
-import menudata from '../megaMenu/megaMenu.data.json';
-
 export default {
   tags: ['autodocs'],
   title: 'Components/Navbar',
@@ -18,15 +14,12 @@ export default {
 export const defaultState = {
   args: { 
     ...default_state,
-    megaMenu: ""
   }
 }
 
 // Default megaMenu story
-const megamenu = new megaMenu(menudata);
-export const WithMegaMenu = {
+export const withMenu = {
   args: { 
     ...menu_state,
-    megaMenu: megamenu.html
   }
 }
