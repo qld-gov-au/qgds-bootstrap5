7ba7080a0e4fe74209d46f67fc6cfe88d3b4163e
Steve Pegg <126438309+stvp-qld@users.noreply.github.com>
2024-04-04 23:02:49 +1000

    QOLOE-19 Added tables to match Design Spec. Removed some Bootstrap defaults that werent required (table-condensed, table-bordered)

--- src/components/bs5/table/Table.stories.js
+++ src/components/bs5/table/Table.stories.js
@@ -6,13 +6,16 @@ export default {
   tags: ["autodocs"],
   title: "Components/Table",
   render: (args) => {
-	
+    
     //Storybook produces a comma delimited string when using the check control type (table-striped, table-bordered) etc. 
     //We can't use commas on our class="..." attribute, so we need to replace the commas with spaces.
-    if( typeof(args.classes) === 'string' ) {
-      args.classes = args.classes.replace(","," ").split(" ");
-    } else if ( typeof(args.classes) === 'object' ) {
-      args.classes = args.classes.join(" ");
+    
+    console.log(args);
+
+    if( typeof(args.variantClass) === 'string' ) {
+      args.variantClass = args.variantClass.replaceAll(","," ");
+    } else if ( typeof(args.variantClass) === 'object' ) {
+      args.variantClass = args.variantClass.join(" ");
     }
 
     return new Table(args).html;
@@ -21,7 +24,7 @@ export default {
 
   //https://storybook.js.org/docs/api/arg-types
   argTypes: {
-    classes: {
+    variantClass: {
       name: "Classes",
       description: `Settable classes for the component`,
       control: {
@@ -29,19 +32,11 @@ export default {
         labels: {
           "table-dark": "Dark",
           "table-striped": "Striped",
-          "table-bordered": "Bordered",
-          "table-borderless": "Borderless",
-          "table-hover": "Hover",
-          "table-sm": "Small",
         },
       },
       options: [
         "table-dark",
         "table-striped",
-        "table-bordered",
-        "table-borderless",
-        "table-hover",
-        "table-sm",
       ],
     },
 
@@ -64,11 +59,11 @@ export default {
   },
 
   parameters: {
-    design: {
-      name: "QGDS Figma Reference",
-      type: "figma",
-      url: "https://www.figma.com/file/qKsxl3ogIlBp7dafgxXuCA/QLD-GOV-DDS?type=design&node-id=23167-395552&mode=design&t=qoWXcELpZe1uqHs8-0",
-    },
+    // design: {
+    //   name: "QGDS Figma Reference",
+    //   type: "figma",
+    //   url: "https://www.figma.com/file/qKsxl3ogIlBp7dafgxXuCA/QLD-GOV-DDS?type=design&node-id=23167-395552&mode=design&t=qoWXcELpZe1uqHs8-0",
+    // },
     docs: {
       controls: {
         exclude: ["headers", "rows", "footer"],
@@ -77,59 +72,136 @@ export default {
   },
 };
 
-// Default story for a Table
+/**
+ * 
+ * For data tables with a small amount of rows use the default table:
+ *  - Align text columns and corresponding data cells to the left
+ *  - When comparing numbers in a column, align data cells and column headers to the right
+ *  - For data tables with more rows, use the striped alternative
+ * 
+ * Example full table including headers, rows and footer, caption and subcaption
+ */
 export const Default = {
   args: defaultdata,
 };
 
-// Default story for a Table
-export const DarkTheme = {
+/**
+ * Table with striping (or banding)  <code>.table-striped</code>
+ */
+
+export const Striped = {
+  args: { 
+    ...defaultdata,
+    ...{variantClass: "table-striped"},
+  },
+};
+  
+
+/**
+ * Also known as: Quiet Table
+ * 
+ * When seeking to integrate and expand the information that accompanies a text, a borderless table can prove advantageous. It imparts a sense of continuity and cohesion, making the data feel more seamlessly integrated with the surrounding text.
+ */
+
+export const Borderless = {
+  args: { 
+	  ...defaultdata,
+	  ...{customClass: "qld-table--borderless"},
+  },
+};
+
+
+/**
+ * Table with row hover <code>.table-hover</code>
+ */
+export const WithHover = {
+  args: { 
+	  ...defaultdata,
+	  ...{variantClass: "table-hover"},
+  },
+};
+
+
+
+/**
+ * Alternate (Dark) table. <code>.table-dark</code>
+ */
+export const Dark = {
   args: {
     ...defaultdata,
-    ...{classes: "table-dark"},
+    ...{variantClass: "table-dark"},
   	},
 }
 
 
-// Default story for a Table
-export const Striped = {
-  args: { 
+/**
+ * Alternate (Dark) table. <code>.table-dark .table-hover</code>
+ */
+export const DarkWithHover = {
+  args: {
     ...defaultdata,
-    ...{classes: "table-striped"},
-  },
-};
+    ...{ variantClass: ["table-dark","table-hover"] },
+  	},
+}
 
 
-// Default story for a Table
-export const Bordered = {
-  args: { 
-	  ...defaultdata,
-	  ...{classes: "table-bordered"},
-  },
-};
-  
-
-// Default story for a Table
-export const Borderless = {
-  args: { 
-	  ...defaultdata,
-	  ...{classes: "table-borderless"},
-  },
-};
-
-
-// Default story for a Table
-export const WithHover = {
-  args: { 
-	  ...defaultdata,
-	  ...{classes: "table-hover"},
-  },
-};
-  
-// Default story for a Table
-export const Small = {
-  args: { 
-	  ...defaultdata,
-	  ...{classes: "table-sm"},
-  },
-};
+/**
+ * Responsive table example <code>.table-responsive</code>
+ */
+export const Responsive = {
+  decorators: [
+    (Story) => {
+      return `
+        <div class="col-6">
+        ${Story()}
+        </div>
+      `;
+    },
+  ],
+  args: {
+    ...defaultdata,
+    ...{ 
+      "headers": [
+        "Header", 
+        "Header", 
+        "Header", 
+        "Header", 
+        "Header", 
+        "Header", 
+        "Header", 
+        "Header", 
+        "Header",
+        "Header",
+        "Header",
+        "Header",
+      ],
+      "rows": [
+        { "cells": ["Cell", "Longer cell", "Cell", "Cell", "Longer cell", "Cell", "Cell", "Cell", "Cell", "Cell", "Longer Cell", "Cell"] },
+        { "cells": ["Cell", "Longer cell", "Cell", "Cell", "Longer cell", "Cell", "Cell", "Cell", "Cell", "Cell", "Longer Cell", "Cell"] },
+        { "cells": ["Cell", "Longer cell", "Cell", "Cell", "Longer cell", "Cell", "Cell", "Cell", "Cell", "Cell", "Longer Cell", "Cell"] },
+        { "cells": ["Cell", "Longer cell", "Cell", "Cell", "Longer cell", "Cell", "Cell", "Cell", "Cell", "Cell", "Longer Cell", "Cell"] },
+        { "cells": ["Cell", "Longer cell", "Cell", "Cell", "Longer cell", "Cell", "Cell", "Cell", "Cell", "Cell", "Longer Cell", "Cell"] },
+        { "cells": ["Cell", "Longer cell", "Cell", "Cell", "Longer cell", "Cell", "Cell", "Cell", "Cell", "Cell", "Longer Cell", "Cell"] },
+      ],
+      "footer": [
+        {
+          "cells": [
+            "Footer",
+            "Footer",
+            "Footer",
+            "Footer",
+            "Footer",
+            "Footer",
+            "Footer",
+            "Footer",
+            "Footer",
+            "Footer",
+            "Footer",
+            "Footer",
+          ]
+        }
+      ]
+    },
+    ...{ variantClass: ["table-striped","table-hover"] },
+  	},
+}
