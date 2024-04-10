203513de658ecdb84d1afd303264c8d69579c626
Mitchell Anderson <mitchellanderson@Mitchells-MacBook-Pro.local>
2024-04-10 14:59:00 +1000

    WIP: Updates

--- src/components/bs5/header/header.stories.js
+++ src/components/bs5/header/header.stories.js
@@ -1,106 +1,134 @@
 // Navbar.stories.js
 import { Header } from './Header.js';
-import { default_state } from './header.data.json';
+import { default_state, sub_brand, co_brand, endorsed, standalone} from './header.data.json';
 
 export default {
-  tags: ['autodocs'],
-  title: 'Components/Header',
-  render: (args) => new Header(args).html,
-  argTypes: {
-  },
+    tags: ['autodocs'],
+    title: 'Components/Header',
+    render: (args) => new Header(args).html,
+    argTypes: {
+    },
 };
 
 // Default navigation story
-export const defaultState = {
-  args: { 
-    ...default_state,
-  }
+export const Masterbrand = {
+    args: {
+        ...default_state,
+    }
+}
+
+// Default navigation story
+export const Subbrand = {
+    args: {
+        ...sub_brand,
+    }
+}
+
+// Default navigation story
+export const Cobrand = {
+    args: {
+        ...co_brand,
+    }
+}
+
+// Default navigation story
+export const Endorsed = {
+    args: {
+        ...endorsed,
+    }
+}
+
+// Default navigation story
+export const Standalone = {
+    args: {
+        ...standalone,
+    }
 }
 
 // Navbar story with 'Light' color theme
 export const LightMode = {
-  args: {
-    // Define args specific to Light mode
-  },
-  parameters: {
-    backgrounds: {
-      default: "Light",
-      values: [{ name: "Light", value: "var(--qld-light-background)" }],
+    args: {
+        // Define args specific to Light mode
     },
-  },
-  decorators: [
-    (Story) => {
-      return `
+    parameters: {
+        backgrounds: {
+            default: "Light",
+            values: [{ name: "Light", value: "var(--qld-light-background)" }],
+        },
+    },
+    decorators: [
+        (Story) => {
+            return `
       <div class="light">
           ${Story()}
       </div>
       `;
-    },
-  ],
+        },
+    ],
 };
 
 // Navbar story with 'Alternative' color theme
 export const Alternative = {
-  args: {
-    // Define args specific to Light mode
-  },
-  parameters: {
-    backgrounds: {
-      default: "Alternative",
-      values: [{ name: "Alternative", value: "var(--qld-light-grey-alt)" }],
+    args: {
+        // Define args specific to Light mode
     },
-  },
-  decorators: [
-    (Story) => {
-      return `
+    parameters: {
+        backgrounds: {
+            default: "Alternative",
+            values: [{ name: "Alternative", value: "var(--qld-light-grey-alt)" }],
+        },
+    },
+    decorators: [
+        (Story) => {
+            return `
       <div class="alt">
           ${Story()}
       </div>
       `;
-    },
-  ],
+        },
+    ],
 };
 
 // Navbar story with 'Dark' color theme
 export const Dark = {
-  args: {
-    // Define args specific to Light mode
-  },
-  parameters: {
-    backgrounds: {
-      default: "Dark",
-      values: [{ name: "Dark", value: "var(--qld-sapphire-blue)" }],
+    args: {
+        // Define args specific to Light mode
     },
-  },
-  decorators: [
-    (Story) => {
-      return `
+    parameters: {
+        backgrounds: {
+            default: "Dark",
+            values: [{ name: "Dark", value: "var(--qld-sapphire-blue)" }],
+        },
+    },
+    decorators: [
+        (Story) => {
+            return `
       <div class="dark">
           ${Story()}
       </div>
       `;
-    },
-  ],
+        },
+    ],
 };
 
 // Navbar story with 'DarkAlternative' color theme
 export const DarkAlternative = {
-  args: {
-    // Define args specific to Light mode
-  },
-  parameters: {
-    backgrounds: {
-      default: "Dark alternative",
-      values: [{ name: "Dark alternative", value: "var(--qld-dark-blue)" }],
+    args: {
+        // Define args specific to Light mode
     },
-  },
-  decorators: [
-    (Story) => {
-      return `
+    parameters: {
+        backgrounds: {
+            default: "Dark alternative",
+            values: [{ name: "Dark alternative", value: "var(--qld-dark-blue)" }],
+        },
+    },
+    decorators: [
+        (Story) => {
+            return `
       <div class="dark-alt">
           ${Story()}
       </div>
       `;
-    },
-  ],
+        },
+    ],
 };
