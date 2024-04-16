a19794cd1804e78f957fbe5507cc11fbd4840efb
Steve Pegg <126438309+stvp-qld@users.noreply.github.com>
2024-04-16 22:04:59 +1000

    QOLOE-9 Renamed Alert to Inpage Alert. Refactored code to align to project standards.

---
+++ src/components/bs5/inpageAlert/inpageAlert.stories.js
@@ -0,0 +1,96 @@
+import { InpageAlert } from "./InpageAlert.js";
+import defaultdata from "./inpageAlert.data.json";
+
+/**
+ * The Inpage Alert is also known as Page Alert or Alert
+ * 
+ * Inpage alert accepts the following classes: <code>.alert-success</code>, <code>.alert-info</code>, <code>.alert-warning</code>, and <code>.alert-danger</code>
+ */
+
+export default {
+  /**
+   * Automatically create documentation (overview) page.
+   *
+   * @type {string[]}
+   */
+  tags: ["autodocs"],
+
+  /**
+   * Name of the story.
+   *
+   * @type {string}
+   */
+  title: "Components/In-page Alert",
+
+  /**
+   * Function that renders the HTML snippet for the story.
+   *
+   * @type {Function}
+   * @param {Object} args - Arguments for rendering the story, including data for the Handlebars template placeholders.
+   * @returns {string} - The rendered HTML snippet.
+   */
+  render: (args) => {
+    return new InpageAlert({ ...defaultdata, ...args }).html;
+  },
+
+  argTypes: {
+    variantClass: {
+      name: "Classes",
+      description: `Settable classes for the Inpage Alert component`,
+      control: {
+        type: "radio",
+        labels: {
+          "alert-success": "Success",
+          "alert-info": "Info",
+          "alert-warning": "Warning",
+          "alert-danger": "Danger",
+        },
+      },
+      options: [
+        "alert-success",
+        "alert-info",
+        "alert-warning",
+        "alert-danger"
+      ],
+    },
+  },
+
+};
+
+
+/**
+ * Default In-page Alert component.
+ */
+
+export const Default = {
+  args: defaultdata,
+};
+
+
+
+
+/**
+ * In-page Alert when presented inside a <code>.dark</code> or <code>.dark-alt</code> container.
+ */
+
+
+export const Dark = {
+  parameters: {
+    backgrounds: {
+      default: 'Dark',
+      values: [
+        { name: 'Dark', value: 'var(--qld-dark-background)' },
+      ],
+    },
+  },
+
+  decorators: [
+    (Story) => {
+      return `
+        <div class="dark">
+          ${Story()}
+        </div>
+      `;
+    },
+  ],
+};
