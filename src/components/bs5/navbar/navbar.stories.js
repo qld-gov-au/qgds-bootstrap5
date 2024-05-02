47b5a9b2e0bf91cdad4f6143ab90a2c747957178
Mitch Anderson <122584447+MitchAnderson93@users.noreply.github.com>
2024-05-02 13:40:26 +1000

    QOLOE-17 Primary navigation + Header (#52)
    * WIP: Primary navigation
    
    * Updated to not include megamenu as seperate component
    
    * Updating navigation prototype
    
    * WIP: Updates
    
    * WIP: Updates
    
    * WIP: Updates
    
    * WIP: Updates
    
    * WIP: Updates
    
    * WIP: Updates
    
    * WIP: Updates
    
    * WIP: Updates
    
    * WIP: Updates
    
    * WIP: Updates
    
    * WIP: Updates
    
    * WIP: Updates
    
    * WIP: Temp working files added
    
    * WIP: Temp files to be removed
    
    * WIP: Working responsive nav styles
    
    * WIP: Updates
    
    * WIP: Updates
    
    * WIP: Updates
    
    * WIP: Update to fix logo
    
    * WIP: Updates
    
    * WIP: Updates
    
    * WIP: Updates
    
    * WIP: Updates
    
    * WIP: Updates
    
    * WIP: Updates
    
    * WIP: Fixing lint errors
    
    * QOLOE-17 temp fix for main.scss to pass build
    
    ---------
    
    Co-authored-by: Mitchell Anderson <mitchellanderson@Mitchells-MacBook-Pro.local>
    Co-authored-by: Steve Pegg <126438309+stvp-qld@users.noreply.github.com>

---
+++ src/components/bs5/navbar/navbar.stories.js
@@ -0,0 +1,120 @@
+// Navbar.stories.js
+import { Navbar } from './Navbar.js';
+import { default_state, menu_state, test_states} from './navbar.data.json';
+
+export default {
+  tags: ['autodocs'],
+  title: 'Components/Navbar',
+  render: (args) => new Navbar(args).html,
+  argTypes: {
+  },
+};
+
+// Default navigation story
+export const DefaultState = {
+  args: { 
+    ...default_state,
+  }
+}
+
+// Default megaMenu story
+export const WithFullMenuOptions = {
+  args: { 
+    ...menu_state,
+  }
+}
+
+// Default megaMenu story
+export const WithMenuNoDescriptions = {
+  args: { 
+    ...test_states,
+  }
+}
+
+// Navbar story with 'Light' color theme
+export const LightMode = {
+  args: {
+    ...menu_state,
+  },
+  parameters: {
+    backgrounds: {
+      default: "Light",
+      values: [{ name: "Light", value: "var(--qld-light-background)" }],
+    },
+  },
+  decorators: [
+    (Story) => {
+      return `
+      <div class="light">
+          ${Story()}
+      </div>
+      `;
+    },
+  ],
+};
+
+// Navbar story with 'Alternative' color theme
+export const Alternative = {
+  args: {
+    ...menu_state,
+  },
+  parameters: {
+    backgrounds: {
+      default: "Alternative",
+      values: [{ name: "Alternative", value: "var(--qld-light-grey-alt)" }],
+    },
+  },
+  decorators: [
+    (Story) => {
+      return `
+      <div class="alt">
+          ${Story()}
+      </div>
+      `;
+    },
+  ],
+};
+
+// Navbar story with 'Dark' color theme
+export const Dark = {
+  args: {
+    ...menu_state,
+  },
+  parameters: {
+    backgrounds: {
+      default: "Dark",
+      values: [{ name: "Dark", value: "var(--qld-sapphire-blue)" }],
+    },
+  },
+  decorators: [
+    (Story) => {
+      return `
+      <div class="dark">
+          ${Story()}
+      </div>
+      `;
+    },
+  ],
+};
+
+// Navbar story with 'DarkAlternative' color theme
+export const DarkAlternative = {
+  args: {
+    ...menu_state,
+  },
+  parameters: {
+    backgrounds: {
+      default: "Dark alternative",
+      values: [{ name: "Dark alternative", value: "var(--qld-dark-blue)" }],
+    },
+  },
+  decorators: [
+    (Story) => {
+      return `
+      <div class="dark-alt">
+          ${Story()}
+      </div>
+      `;
+    },
+  ],
+};
