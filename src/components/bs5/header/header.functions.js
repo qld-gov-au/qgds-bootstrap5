e692f669fa44c5b79311f705d5cc50163b5f53ac
mitch <mitch-anderson@outlook.com>
2024-05-10 10:18:45 +1000

    WIP: Updates

--- src/components/bs5/header/header.functions.js
+++ src/components/bs5/header/header.functions.js
@@ -8,24 +8,23 @@
  */
 export function toggleSearch(event) {
 
-    // Prevent default action and stop event propagation
-    try {
-      event.preventDefault();
-      event.stopPropagation();
-    } catch (error) {
-      console.error("Error in event handling:", error);
-    }
-  
-    // Get the search div
-    const searchDiv = document.getElementById('qld-header-search');
-  
-    // Toggle the class for the open state
-    if (searchDiv) {
-      searchDiv.classList.toggle('qld__header__site-search--open');
-  
-      // Optional: Update the aria-expanded attribute for accessibility
-      const isExpanded = event.currentTarget.getAttribute('aria-expanded') === 'true';
-      event.currentTarget.setAttribute('aria-expanded', !isExpanded);
-    }
+  // Prevent default action and stop event propagation
+  try {
+    event.preventDefault();
+    event.stopPropagation();
+  } catch (error) {
+    console.error("Error in event handling:", error);
   }
-  
+
+  // Get the search div
+  const searchDiv = document.getElementById('qld-header-search');
+
+  // Toggle the class for the open state
+  if (searchDiv) {
+    searchDiv.classList.toggle('qld__header__site-search--open');
+
+    // Optional: Update the aria-expanded attribute for accessibility
+    const isExpanded = event.currentTarget.getAttribute('aria-expanded') === 'true';
+    event.currentTarget.setAttribute('aria-expanded', !isExpanded);
+  }
+}
