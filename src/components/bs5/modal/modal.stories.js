fb4db471210aed7a3020f09c53b478459d9702f1
Steve Pegg <126438309+stvp-qld@users.noreply.github.com>
2024-05-13 23:41:14 +1000

    QOLOE-117 Add modal component, bugfix for buttons, header

---
+++ src/components/bs5/modal/modal.stories.js
@@ -0,0 +1,116 @@
+// modal.stories.js
+import { Modal } from './Modal.js';
+import defaultdata from './modal.data.json';
+
+//Load some buttons for modal footer
+import { Button } from '../button/Button.js';
+
+let buttonItems = [{    
+  "variantClass": "btn-tertiary",
+  "islink": true,
+  "isdisabled": false,
+  "iconClass": "",
+  "href": "https:///www.google.com",
+  "label": "External link",
+  "target": "_blank",
+  "dataatts": false,
+  "arialabel": false,
+},{    
+  "variantClass": "btn-secondary",
+  "islink": false,
+  "isdisabled": false,
+  "iconClass": "",
+  "href": "#",
+  "label": "Cancel",
+  "target": "",
+  "dataatts": "data-bs-dismiss='modal'",
+  "arialabel": "Close",
+},
+{    
+  "variantClass": "btn-primary",
+  "islink": false,
+  "isdisabled": false,
+  "iconClass": "",
+  "href": "#",
+  "label": "Confirm",
+  "target": "",
+  "dataatts": "data-bs-dismiss='modal'",
+  "arialabel": "Close",
+}];
+
+defaultdata.footer.buttons = buttonItems.map((item) => { 
+  return new Button(item).html;
+});
+
+
+export default {
+  tags: [''],
+  title: 'Components/Modal',
+  render: (args) => new Modal(args).html,
+
+  argTypes: {
+    buttons: { 
+      control: { disable: true },
+    },
+    modalSize: {
+      name: "Size",
+      description: `Set the size of the modal`,
+      control: {
+        type: "select",
+        labels: {
+          "modal-sm": "Small",
+          "modal-default": "Default",
+          "modal-lg": "Large",
+          "modal-xl": "Extra Large",
+        },
+      },
+      options: [
+        "modal-sm",
+        "modal-default",
+        "modal-lg",
+        "modal-xl",
+      ],
+    },
+  },
+};
+
+/**
+ * Default Modal (Extra Large, 1140px)
+ */
+export const Default = {
+  args: {
+    ...defaultdata,
+    modalID: 'modal-example-default',
+    modalLabel: 'Default modal example',
+    modalSize: 'modal-xl',
+    launchButtonLabel: 'Open default modal',
+  },
+};
+
+/**
+ * Small Modal (575px)
+ */
+
+export const SmallModal= {
+  args: {
+    ...defaultdata,
+    modalID: 'modal-example-small',
+    modalLabel: 'Small modal example',
+    modalSize: 'modal-sm',
+    launchButtonLabel: 'Open small modal',
+  },
+};
+
+
+/**
+ * Large Modal (800px)
+ */
+export const LargeModal = {
+  args: {
+    ...defaultdata,
+    modalID: 'modal-example-large',
+    modalLabel: 'Large modal example',
+    modalSize: 'modal-lg',
+    launchButtonLabel: 'Open large modal',
+  },
+};
--- src/components/bs5/navbar/navbar.functions.js
+++ src/components/bs5/navbar/navbar.functions.js
@@ -39,57 +39,66 @@ export function initializeNavbar() {
   const navbarCollapse = document.getElementById('navbarSupportedContent');
   const overlay = document.getElementById('overlay');
 
-  overlay.addEventListener('click', function () {
-    // Check if the navbar is open
-    if (navbarCollapse.classList.contains('show')) {
-      // If open, close it
-      navbarCollapse.classList.remove('show');
-      overlay.classList.remove('show');
-    }
-  });
+  if(overlay) {
+    
+    overlay.addEventListener('click', function () {
+      // Check if the navbar is open
+      if (navbarCollapse.classList.contains('show')) {
+        // If open, close it
+        navbarCollapse.classList.remove('show');
+        overlay.classList.remove('show');
+      }
+    });
+  }
 
   // Overlay show/hide events
-  navbarCollapse.addEventListener('show.bs.collapse', function () {
-    overlay.classList.add('show'); // Show the overlay
-  });
+  if(navbarCollapse) {
+    
+    navbarCollapse.addEventListener('show.bs.collapse', function () {
+      overlay.classList.add('show'); // Show the overlay
+    });
+    
+    navbarCollapse.addEventListener('hide.bs.collapse', function () {
+      overlay.classList.remove('show'); // Hide the overlay
+    });
 
-  navbarCollapse.addEventListener('hide.bs.collapse', function () {
-    overlay.classList.remove('show'); // Hide the overlay
-  });
+  }
 
   // Initialize Popper and toggle handling
-  dropdownToggles.forEach(toggle => {
-    toggle.addEventListener('click', (event) => {
-      event.preventDefault();
-      const parentUl = toggle.closest('ul.navbar-nav');
-      const firstLi = parentUl.querySelector('.nav-item-home');
-      const dropdownMenu = toggle.parentNode.querySelector('.dropdown-menu');
+  if(dropdownToggles.length > 0) {
+    dropdownToggles.forEach(toggle => {
+      toggle.addEventListener('click', (event) => {
+        event.preventDefault();
+        const parentUl = toggle.closest('ul.navbar-nav');
+        const firstLi = parentUl.querySelector('.nav-item-home');
+        const dropdownMenu = toggle.parentNode.querySelector('.dropdown-menu');
 
-      // Check if a Popper instance should be activated or not
-      const resizeHandler = () => {
-        if (window.innerWidth > 992) {
-          if (!dropdownMenu.dataset.popperActive) {
-            let popperInstance = initializeDropdownPopper(firstLi, dropdownMenu);
-            dropdownMenu.dataset.popperActive = 'true';
-            activePoppers.push(popperInstance);
+        // Check if a Popper instance should be activated or not
+        const resizeHandler = () => {
+          if (window.innerWidth > 992) {
+            if (!dropdownMenu.dataset.popperActive) {
+              let popperInstance = initializeDropdownPopper(firstLi, dropdownMenu);
+              dropdownMenu.dataset.popperActive = 'true';
+              activePoppers.push(popperInstance);
+            } else {
+              // If the Popper instance exists, update its position
+              activePoppers.forEach(popperInstance => {
+                popperInstance.update();
+              });
+            }
           } else {
-            // If the Popper instance exists, update its position
-            activePoppers.forEach(popperInstance => {
-              popperInstance.update();
-            });
-          }
-        } else {
-          if (dropdownMenu.dataset.popperActive) {
-            activePoppers.forEach(popperInstance => popperInstance.destroy());
-            activePoppers = []; // Clear out the array after destroying instances
-            delete dropdownMenu.dataset.popperActive;
+            if (dropdownMenu.dataset.popperActive) {
+              activePoppers.forEach(popperInstance => popperInstance.destroy());
+              activePoppers = []; // Clear out the array after destroying instances
+              delete dropdownMenu.dataset.popperActive;
+            }
           }
         }
-      }
 
-      // Attach resize listener to update Popper on resize
-      window.addEventListener('resize', resizeHandler);
-      resizeHandler(); // Call handler immediately to apply correct setting on init
+        // Attach resize listener to update Popper on resize
+        window.addEventListener('resize', resizeHandler);
+        resizeHandler(); // Call handler immediately to apply correct setting on init
+      });
     });
-  });
+  }
 }
