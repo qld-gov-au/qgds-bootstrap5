4dd22982bcdc8e78a67f8da80822571f8b9e50ca
mitch <mitch-anderson@outlook.com>
2024-05-16 15:38:06 +1000

    WIP: Updates - swap icon when expanded

--- src/components/bs5/header/header.functions.js
+++ src/components/bs5/header/header.functions.js
@@ -18,15 +18,27 @@ export function toggleSearch(event) {
 
   // Get the search div
   const searchDiv = document.getElementById('qld-header-search');
-
+  const toggleButton = event.currentTarget;
+  const toggleIcon = toggleButton.querySelector('svg use');
+  const toggleText = toggleButton.querySelector('.qld__main-nav__toggle-text');
+  
   // Check current class and swap
   if (searchDiv) {
     if (searchDiv.classList.contains('qld__header__site-search--open')) {
       searchDiv.classList.remove('qld__header__site-search--open');
       searchDiv.classList.add('qld__header__site-search--closed');
+
+      // TO DO: Review - Change icon and text back to default/no root reference to file?
+      toggleIcon.setAttribute('href', 'assets/img/svg-icons.svg#qld__icon__search');
+      toggleText.textContent = 'Search';
+
     } else {
       searchDiv.classList.remove('qld__header__site-search--closed');
       searchDiv.classList.add('qld__header__site-search--open');
+
+      // TO DO: Change icon and text to active state/no root reference to file?
+      toggleIcon.setAttribute('href', 'assets/img/svg-icons.svg#qld__icon__close'); 
+      toggleText.textContent = 'Close';
     }
 
     // Optional: Update the aria-expanded attribute for accessibility
