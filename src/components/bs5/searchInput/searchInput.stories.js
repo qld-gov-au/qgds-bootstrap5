d4806b3be968064311418541ed9eecdc32068442
Steve Pegg <126438309+stvp-qld@users.noreply.github.com>
2024-05-22 22:33:25 +1000

    QOLOE-127 Adds generic Search Input field for site search

---
+++ src/components/bs5/searchInput/searchInput.stories.js
@@ -0,0 +1,72 @@
+// SearchInput.stories.js
+import { SearchInput } from "./SearchInput.js";
+import defaultdata from "./searchInput.data.json";
+
+export default {
+  tags: ["autodocs"],
+  title: "Components/Search Input",
+  render: (args) => {
+    return new SearchInput(args).html;
+  },
+
+  argTypes: {
+  },
+
+  parameters: {
+    docs: {
+      controls: {
+        exclude: ["variantClass", "customClass", "inputID", "inputName", "buttonID", "buttonType", "ariaLabel"],
+      },
+    },
+  },
+
+};
+
+
+
+
+/**
+ * Default Search Input
+ */
+export const Default = {
+  args: defaultdata,
+};
+
+/**
+ * Full width Search Input
+ */
+export const FullWidth = {
+  args: {
+    ...defaultdata,
+    customClass: "full-width",
+  },
+}
+
+
+/**
+ * Dark themed Search Input
+ * 
+ * Apply a class <code>.dark</code> to the parent container of the <code>.qld-search-input</code> element. 
+ */
+
+export const Dark = {
+  args: { 
+	  ...defaultdata,
+	  variantClass: "dark",
+  },
+  parameters: {
+    backgrounds: {
+      default: 'Dark',
+      values: [
+        { name: 'Dark', value: 'var(--qld-brand-primary)' },
+      ],
+    },
+  },
+  decorators: [
+    (Story) => {
+      return `
+          ${Story()}
+      `;
+    },
+  ],
+};
