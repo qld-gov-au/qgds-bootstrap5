7034f36cc4adad989b75885e993da07d3be06e38
Mitch Anderson <122584447+MitchAnderson93@users.noreply.github.com>
2024-05-23 12:25:04 +1000

    WIP: Header pattern - defects/gaps (v5.0) (#138)
    * WIP: Updates

--- src/components/bs5/navbar/navbar.functions.js
+++ src/components/bs5/navbar/navbar.functions.js
@@ -42,69 +42,98 @@ export function initializeNavbar() {
   const overlay = document.getElementById('overlay');
 
   // Add event listeners to each dropdown item
-  dropdownItems.forEach(item => {
-    item.addEventListener('click', function(event) {
-      // Stop the event from propagating - Bootstrap default click closes
-      event.stopPropagation();  
+  if(dropdownItems) {
+    dropdownItems.forEach(item => {
+      item.addEventListener('click', function(event) {
+        // Stop the event from propagating - Bootstrap default click closes
+        event.stopPropagation();  
+      });
     });
-  });
+  }
 
   // Overlay on mobile open
-  overlay.addEventListener('click', function () {
-    // Check if the navbar is open
-    if (navbarCollapse.classList.contains('show')) {
-      // If open, close it
-      navbarCollapse.classList.remove('show');
-      overlay.classList.remove('show');
-    }
-  });
+  if(overlay) {
+    overlay.addEventListener('click', function () {
+      // Check if the navbar is open
+      if (navbarCollapse.classList.contains('show')) {
+        // If open, close it
+        navbarCollapse.classList.remove('show');
+        overlay.classList.remove('show');
+      }
+    });
+  }
 
-  // Overlay show/hide events
-  navbarCollapse.addEventListener('show.bs.collapse', function () {
-    overlay.classList.add('show'); // Show the overlay
-  });
+  if(navbarCollapse) {
+    // Overlay show/hide events
+    navbarCollapse.addEventListener('show.bs.collapse', function () {
+      overlay.classList.add('show'); // Show the overlay
+    });
 
-  navbarCollapse.addEventListener('hide.bs.collapse', function () {
-    overlay.classList.remove('show'); // Hide the overlay
-  });
+    navbarCollapse.addEventListener('hide.bs.collapse', function () {
+      overlay.classList.remove('show'); // Hide the overlay
+    });
+  }
 
-  // Initialize Popper and toggle handling
-  dropdownToggles.forEach(toggle => {
-    toggle.addEventListener('click', (event) => {
-      event.preventDefault();
-      const parentUl = toggle.closest('ul.navbar-nav');
-      const firstLi = parentUl.querySelector('.nav-item-home');
-      const dropdownMenu = toggle.parentNode.querySelector('.dropdown-menu');
-     
-      // Check if a Popper instance should be activated or not
-      const resizeHandler = () => {
-        if (window.innerWidth > 992) {
-          if (!dropdownMenu.dataset.popperActive) {
-            let popperInstance = initializeDropdownPopper(firstLi, dropdownMenu);
-            console.log(popperInstance);
-            dropdownMenu.dataset.popperActive = 'true';
-            activePoppers.push(popperInstance);
-          } else {
-            // If the Popper instance exists, update its position
-            activePoppers.forEach(popperInstance => {
-              popperInstance.update();
-            });
+  if(dropdownToggles) {
+    // Initialize Popper and toggle handling
+    dropdownToggles.forEach(toggle => {
+      toggle.addEventListener('click', (event) => {
+        event.preventDefault();
+        const parentUl = toggle.closest('ul.navbar-nav');
+        const firstLi = parentUl.querySelector('.nav-item-home');
+        const dropdownMenu = toggle.parentNode.querySelector('.dropdown-menu');
+        const siblingElement = toggle.closest('.nav-item').querySelector('.first-element');
+
+        // Remove 'show' class from all dropdowns
+        document.querySelectorAll('.navbar .dropdown-menu').forEach(menu => {
+          if (menu !== dropdownMenu) {
+            menu.classList.remove('show');
           }
+        });
+
+        // Remove 'show' class from all sibling elements
+        document.querySelectorAll('.navbar .first-element').forEach(elem => {
+          if (elem !== siblingElement) {
+            elem.classList.remove('show');
+          }
+        });
+
+        // Toggle the 'show' class on the sibling element
+        if (dropdownMenu.classList.contains('show')) {
+          siblingElement.classList.add('show');
         } else {
-          const dropdownMenu = toggle.closest('.nav-item').querySelector('.dropdown-menu');
-          if (dropdownMenu.dataset.popperActive) {
-            activePoppers.forEach(popperInstance => popperInstance.destroy());
-            activePoppers = []; // Clear out the array after destroying instances
-            delete dropdownMenu.dataset.popperActive;
+          siblingElement.classList.remove('show');
+        }
+        
+        // Check if a Popper instance should be activated or not
+        const resizeHandler = () => {
+          if (window.innerWidth > 992) {
+            if (!dropdownMenu.dataset.popperActive) {
+              let popperInstance = initializeDropdownPopper(firstLi, dropdownMenu);
+              dropdownMenu.dataset.popperActive = 'true';
+              activePoppers.push(popperInstance);
+            } else {
+              // If the Popper instance exists, update its position
+              activePoppers.forEach(popperInstance => {
+                popperInstance.update();
+              });
+            }
+          } else {
+            const dropdownMenu = toggle.closest('.nav-item').querySelector('.dropdown-menu');
+            if (dropdownMenu.dataset.popperActive) {
+              activePoppers.forEach(popperInstance => popperInstance.destroy());
+              activePoppers = []; // Clear out the array after destroying instances
+              delete dropdownMenu.dataset.popperActive;
+            }
           }
         }
-      }
 
-      // Attach resize listener to update Popper on resize
-      window.addEventListener('resize', resizeHandler);
-      resizeHandler(); // Call handler immediately to apply correct setting on init
+        // Attach resize listener to update Popper on resize
+        window.addEventListener('resize', resizeHandler);
+        resizeHandler(); // Call handler immediately to apply correct setting on init
+      });
     });
-  });
+  }
 }
 
 // Initialize the navbar (see main.js)
