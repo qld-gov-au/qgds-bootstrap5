1784d6d264863513b4f996d60edd5d42b2dd098b
Elvi Shu <elvi.shu@smartservice.qld.gov.au>
2024-06-05 16:44:38 +1000

    [QOLOE-198]Handling js error when breadcrumb component does not exist.

--- src/components/bs5/breadcrumbs/breadcrumb.functions.js
+++ src/components/bs5/breadcrumbs/breadcrumb.functions.js
@@ -1,37 +1,60 @@
 /**
+ * Initialise the Breadcrumb component.
+ * Shorten long breadcrumbs when required.
+ *
+ * @memberof module:Breadcrumb
+ *
+ * @returns {void} Returns early when breadcrumb does not exist or its length is within set maxLength. 
+ */
+export function initBreadcrumb() {
+  // Set the standard breadcrumb length.
+  const maxLength = 4;
+  // Get the breadcrumb DOM element.
+  const breadcrumbList = document.querySelector('.breadcrumb').querySelectorAll('.breadcrumb-item');
+  // Return when breadcrumb does not exist.
+  if (!breadcrumbList || !breadcrumbList.length) {
+    return;
+  }
+  // Shorten breadcrumb.
+  breadcrumbShorten(breadcrumbList, maxLength);
+}
+
+/**
  * Shorten long breadcrumb lists
  *
  * @memberof module:Breadcrumb
  *
- * @param  {Object}  event - The event that triggered this function.
- * @returns {void}
+ * @param  {Element} breadcrumbList - Breadcrumb DOM element.
+ * @param  {number} maxLength - Standard maximum length for breadcrumb.
+ * @returns {void} Returns early when breadcrumb does not exist or its length is within set maxLength.
  */
-export function breadcrumbShorten () {
-  let breadcrumbList = document.querySelector('.breadcrumb').querySelectorAll('.breadcrumb-item')
-
-  if (breadcrumbList.length > 4) {
-    breadcrumbList.forEach((crumb, index) => {
-      if (index > 1 && index < breadcrumbList.length - 2) {
-        crumb.classList.add('shortened')
-        crumb.querySelector('a').setAttribute('tabindex',-1)
-      }
-
-      if (index === 1) {
-        let expandCrumb = document.createElement('li'),
-          expandButton = document.createElement('a')
-
-        expandCrumb.classList.add('breadcrumb-item','breadcrumb-toggle')
-
-        expandButton.setAttribute('href', 'javascript:void(0)')
-        expandButton.setAttribute('aria-label', 'Expand the breadcrumbs')
-        expandButton.textContent = '[...]'
-        expandButton.addEventListener('click', breadcrumbExpand)
-
-        expandCrumb.appendChild(expandButton)
-        crumb.after(expandCrumb)
-      }
-    })
+export function breadcrumbShorten(breadcrumbList, maxLength = 4) {
+  // No shortening is required when breadcrumb does not exist or its length is within the maximum range.
+  if (!breadcrumbList || breadcrumbList.length <= maxLength) {
+    return;
   }
+
+  breadcrumbList.forEach((crumb, index) => {
+    if (index > 1 && index < breadcrumbList.length - 2) {
+      crumb.classList.add('shortened')
+      crumb.querySelector('a').setAttribute('tabindex',-1)
+    }
+
+    if (index === 1) {
+      let expandCrumb = document.createElement('li'),
+        expandButton = document.createElement('a')
+
+      expandCrumb.classList.add('breadcrumb-item','breadcrumb-toggle')
+
+      expandButton.setAttribute('href', 'javascript:void(0)')
+      expandButton.setAttribute('aria-label', 'Expand the breadcrumbs')
+      expandButton.textContent = '[...]'
+      expandButton.addEventListener('click', breadcrumbExpand)
+
+      expandCrumb.appendChild(expandButton)
+      crumb.after(expandCrumb)
+    }
+  })
 }
 
 /**
@@ -39,12 +62,17 @@ export function breadcrumbShorten () {
  *
  * @memberof module:Breadcrumb
  *
- * @param  {Object}  event - The event that triggered this function.
- * @returns {void}
+ * @param  {Event} event - The event that triggered this function.
+ * @returns {void} Returns early when the breadcrumb does not exist or is empty.
  */
-export function breadcrumbExpand () {
-  let breadcrumbList = document.querySelector('.breadcrumb').querySelectorAll('.breadcrumb-item')
+export function breadcrumbExpand(event) {
+  const breadcrumbList = event.target.closest('.breadcrumb').querySelectorAll('.breadcrumb-item');
 
+  if (!breadcrumbList || !breadcrumbList.length) {
+    console.log('Breadcrumb does not exist or is empty.');
+    return;
+  }
+  
   breadcrumbList[0].parentElement.classList.add('expanded')
 
   breadcrumbList.forEach((crumb, index) => {
