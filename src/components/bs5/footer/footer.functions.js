17a89fe90210f1f465d78536e5bc357f07244877
Sen Chung <sen.chung@smartservice.qld.gov.au>
2024-06-07 15:45:40 +1000

    WIP: QOLOE-22-Footer Js Form IO init will always load

--- src/components/bs5/footer/footer.functions.js
+++ src/components/bs5/footer/footer.functions.js
@@ -6,6 +6,26 @@
  * @returns {void}
  */
 export function displayFeedbackForm() {
-  // eslint-disable-next-line no-undef
-  FormioScript.init();
+  if (typeof FormioScript === "object") {
+    // eslint-disable-next-line no-undef
+    FormioScript.init();
+  } else {
+    let scriptEle = document.createElement("script");
+    scriptEle.setAttribute("src", "https://static.qgov.net.au/formio-qld/v2/v2.x.x-latest/formio-script.min.js");
+    scriptEle.setAttribute("type", "text/javascript");
+    scriptEle.setAttribute("async", true);
+
+    document.body.appendChild(scriptEle);
+
+    scriptEle.addEventListener("load", () => {
+      // eslint-disable-next-line no-undef
+      FormioScript.init();
+    });
+    // error event
+    if (location.hostname === "localhost" || location.hostname === "127.0.0.1") {
+      scriptEle.addEventListener("error", (ev) => {
+        console.log("Error on loading file", ev);
+      });
+    }
+  }
 }
