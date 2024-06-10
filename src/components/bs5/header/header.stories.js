c8a9375064102ba55160b792b21e785fca5db099
Mitch Anderson <mitch-anderson@outlook.com>
2024-06-10 17:08:08 +1000

    WIP: Added colour controls

--- src/components/bs5/header/header.stories.js
+++ src/components/bs5/header/header.stories.js
@@ -1,11 +1,10 @@
-// Navbar.stories.js
 import { Header } from './Header.js';
 import { Navbar } from '../navbar/Navbar.js';
 import { SearchInput } from "../searchInput/SearchInput.js";
 
-//Sample components
-import { Banner } from '../banner/Banner.js'; 
-import { Footer } from '../footer/Footer.js'; 
+// Sample components
+import { Banner } from '../banner/Banner.js';
+import { Footer } from '../footer/Footer.js';
 
 // Mock data
 import { menu_state } from '../navbar/navbar.data.json';
@@ -13,25 +12,25 @@ import searchData from '../searchInput/searchInput.data.json';
 import breadcrumbs from '../banner/banner.data.json';
 import footerData from '../footer/footer.data.json';
 
-import { 
-  masterbrand_variant, 
-  subbrand_variant, 
-  endorsed_variant, 
-  standalone_variant } from './header.data.json';
+import {
+  masterbrand_variant,
+  subbrand_variant,
+  endorsed_variant,
+  standalone_variant
+} from './header.data.json';
 
 export default {
   tags: ['autodocs'],
   title: 'Components/Header',
   render: (args) => {
     return `
-        ${new Header({...args, searchInput: new SearchInput(searchData).html}).html}
+        ${new Header({ ...args, searchInput: new SearchInput(searchData).html }).html}
         ${new Navbar(args).html}
-        `//expand arguments, specifically turn isdisabled into true
+        `;
   },
-  argTypes: {
-  },
+  argTypes: {},
   parameters: {
-    layout: 'fullscreen',  
+    layout: 'fullscreen',
   },
 };
 
@@ -109,6 +108,80 @@ export const StandaloneBrand = {
   args: {
     ...standalone_variant,
     ...menu_state,
+    '--qld-color-default-color-light-background-default': '#ffffff',
+    '--qld-color-default-color-light-background-default-shade': '#f5f5f5',
+    '--qld-color-default-color-light-background-light': '#eff4f9',
+    '--qld-color-default-color-light-background-light-shade': '#e5eef5',
+    '--qld-color-default-color-light-background-alt': '#f4f4f4',
+    '--qld-color-default-color-light-background-alt-shade': '#d6eff4',
+    '--qld-color-default-color-light-border-default': '#ebebeb',
+    '--qld-color-default-color-light-border-light': '#ccddee',
+    '--qld-color-default-color-light-border-alt': '#6F8690',
+    '--qld-color-default-color-light-action-primary': '#06658A',
+    '--qld-color-default-color-light-action-primary-hover': '#161616',
+    '--qld-color-default-color-light-action-secondary': '#009BAD',
+    '--qld-color-default-color-light-action-secondary-hover': '#161616',
+    '--qld-color-default-color-light-link-on-action': '#06658A',
+    '--qld-color-default-color-light-link-default': '#046994',
+    '--qld-color-default-color-light-link-visited': '#551A8B',
+    '--qld-color-default-color-light-accent-design-accent': '#FFD559',
+    '--qld-color-default-color-light-focus-default': '#02A2B5',
+    '--qld-color-default-color-light-underline-default': '#1B88B7',
+    '--qld-color-default-color-light-underline-default-hover': '#09549F',
+    '--qld-color-default-color-light-underline-visited': '#8b63b0',
+    '--qld-color-default-color-light-underline-visited-hover': '#551a8b',
+    '--qld-color-default-color-light-text-default': '#333333',
+    '--qld-color-default-color-light-text-lighter': '#636363',
+    '--qld-color-default-color-light-text-heading': '#003549',
+    '--qld-color-default-color-light-site-title': '#022A50',
+    '--qld-color-default-color-light-crest-fill': '#022A50',
+    '--qld-color-default-color-dark-background-default': '#046994',
+    '--qld-color-default-color-dark-background-default-shade': '#005C84',
+    '--qld-color-default-color-dark-background-alt': '#080707',
+    '--qld-color-default-color-dark-background-alt-shade': '#161616',
+    '--qld-color-default-color-dark-border-default': '#4A93B3',
+    '--qld-color-default-color-dark-border-alt': '#09ACFE',
+    '--qld-color-default-color-dark-action-primary': '#4A93B3',
+    '--qld-color-default-color-dark-action-primary-hover': '#FFFFFF',
+    '--qld-color-default-color-dark-action-secondary': '#FFD559'
+  },
+  argTypes: {
+    '--qld-color-default-color-light-background-default': { control: 'color' },
+    '--qld-color-default-color-light-background-default-shade': { control: 'color' },
+    '--qld-color-default-color-light-background-light': { control: 'color' },
+    '--qld-color-default-color-light-background-light-shade': { control: 'color' },
+    '--qld-color-default-color-light-background-alt': { control: 'color' },
+    '--qld-color-default-color-light-background-alt-shade': { control: 'color' },
+    '--qld-color-default-color-light-border-default': { control: 'color' },
+    '--qld-color-default-color-light-border-light': { control: 'color' },
+    '--qld-color-default-color-light-border-alt': { control: 'color' },
+    '--qld-color-default-color-light-action-primary': { control: 'color' },
+    '--qld-color-default-color-light-action-primary-hover': { control: 'color' },
+    '--qld-color-default-color-light-action-secondary': { control: 'color' },
+    '--qld-color-default-color-light-action-secondary-hover': { control: 'color' },
+    '--qld-color-default-color-light-link-on-action': { control: 'color' },
+    '--qld-color-default-color-light-link-default': { control: 'color' },
+    '--qld-color-default-color-light-link-visited': { control: 'color' },
+    '--qld-color-default-color-light-accent-design-accent': { control: 'color' },
+    '--qld-color-default-color-light-focus-default': { control: 'color' },
+    '--qld-color-default-color-light-underline-default': { control: 'color' },
+    '--qld-color-default-color-light-underline-default-hover': { control: 'color' },
+    '--qld-color-default-color-light-underline-visited': { control: 'color' },
+    '--qld-color-default-color-light-underline-visited-hover': { control: 'color' },
+    '--qld-color-default-color-light-text-default': { control: 'color' },
+    '--qld-color-default-color-light-text-lighter': { control: 'color' },
+    '--qld-color-default-color-light-text-heading': { control: 'color' },
+    '--qld-color-default-color-light-site-title': { control: 'color' },
+    '--qld-color-default-color-light-crest-fill': { control: 'color' },
+    '--qld-color-default-color-dark-background-default': { control: 'color' },
+    '--qld-color-default-color-dark-background-default-shade': { control: 'color' },
+    '--qld-color-default-color-dark-background-alt': { control: 'color' },
+    '--qld-color-default-color-dark-background-alt-shade': { control: 'color' },
+    '--qld-color-default-color-dark-border-default': { control: 'color' },
+    '--qld-color-default-color-dark-border-alt': { control: 'color' },
+    '--qld-color-default-color-dark-action-primary': { control: 'color' },
+    '--qld-color-default-color-dark-action-primary-hover': { control: 'color' },
+    '--qld-color-default-color-dark-action-secondary': { control: 'color' },
   },
   parameters: {
     backgrounds: {
@@ -119,94 +192,28 @@ export const StandaloneBrand = {
     },
   },
   decorators: [
-    (Story) => {
+    (Story, context) => {
+      const { variantClass, ...args } = context.args;
+      const bannerDataWithOverride = { ...breadcrumbs, variantClass };
+      const footerDataWithOverride = { ...footerData, variantClass };
+
+      // Generate CSS variables from args
+      const cssVariables = Object.keys(args).filter(key => key.startsWith('--')).map(key => {
+        return `${key}: ${args[key]};`;
+      }).join('\n');
+
       return `
         <style>
         :root {
-          --qld-core-default-color-neutral-black: #131212;
-          --qld-core-default-color-neutral-darkest: #222020;
-          --qld-core-default-color-neutral-darker: #444444;
-          --qld-core-default-color-neutral-dark: #78797e;
-          --qld-core-default-color-neutral-light: #e0e0e0;
-          --qld-core-default-color-neutral-lighter: #ebebeb;
-          --qld-core-default-color-neutral-lightest: #f5f5f5;
-          --qld-core-default-color-neutral-white: #ffffff;
-          --qld-core-default-color-status-caution-default: #ffcc2c;
-          --qld-core-default-color-status-caution-darker: #B38800;
-          --qld-core-default-color-status-caution-lighter: #fff2c9;
-          --qld-core-default-color-status-caution-lightest: #fffaea;
-          --qld-core-default-color-status-info-default: #0085b3;
-          --qld-core-default-color-status-info-darker: #006a8f;
-          --qld-core-default-color-status-info-lighter: #e5eef5;
-          --qld-core-default-color-status-info-lightest: #eff4f9;
-          --qld-core-default-color-status-error-default: #e22339;
-          --qld-core-default-color-status-error-darker: #8a1220;
-          --qld-core-default-color-status-error-lighter: #fdf0f0;
-          --qld-core-default-color-status-error-lightest: #fff6f6;
-          --qld-core-default-color-status-success-default: #339d37;
-          --qld-core-default-color-status-success-darker: #0a690d;
-          --qld-core-default-color-status-success-lighter: #f2faf4;
-          --qld-core-default-color-status-success-lightest: #f7fbf8;
-          --qld-core-default-color-status-underline-light: #ffffffb8;
-          --qld-core-default-color-status-underline-dark: #03213fb8;
-          --qld-core-default-color-brand-primary-dark-blue: #05325f;
-          --qld-core-default-color-brand-primary-sapphire-blue: #09549f;
-          --qld-core-default-color-brand-primary-light-blue: #0085b3;
-          --qld-core-default-color-brand-primary-light-green: #6bbe27;
-          --qld-core-default-color-brand-secondary-darkgreen: #008635;
-          --qld-core-default-color-brand-secondary-golden-yellow: #ffe500;
-          --qld-core-default-color-brand-other-modern-maroon: #a70240;
-          --qld-core-default-color-brand-other-gov-maroon: #771e32;
-          --qld-core-default-color-code-green: #6bbe27;
-          --qld-core-default-color-code-light-blue: #a7e5ff;
-          --qld-core-default-color-code-violet: #c88df7;
-          --qld-core-default-color-code-muted: #c0d7ec;
-          --qld-core-default-color-contained-layout-background: #09549F;
-          --qld-core-default-color-contained-layout-background-dark: #09549F;
-          --qld-color-default-color-light-background-default: #ffffff;
-          --qld-color-default-color-light-background-default-shade: #f5f5f5;
-          --qld-color-default-color-light-background-light: #eff4f9;
-          --qld-color-default-color-light-background-light-shade: #e5eef5;
-          --qld-color-default-color-light-background-alt: #f4f4f4;
-          --qld-color-default-color-light-background-alt-shade: #d6eff4;
-          --qld-color-default-color-light-border-default: #ebebeb;
-          --qld-color-default-color-light-border-light: #ccddee;
-          --qld-color-default-color-light-border-alt: #6F8690;
-          --qld-color-default-color-light-action-primary: #06658A;
-          --qld-color-default-color-light-action-primary-hover: #161616;
-          --qld-color-default-color-light-action-secondary: #009BAD;
-          --qld-color-default-color-light-action-secondary-hover: #161616;
-          --qld-color-default-color-light-link-on-action: #06658A;
-          --qld-color-default-color-light-link-default: #046994;
-          --qld-color-default-color-light-link-visited: #551A8B;
-          --qld-color-default-color-light-accent-design-accent: #FFD559;
-          --qld-color-default-color-light-focus-default: #02A2B5;
-          --qld-color-default-color-light-underline-default: #1B88B7;
-          --qld-color-default-color-light-underline-default-hover: #09549F;
-          --qld-color-default-color-light-underline-visited: #8b63b0;
-          --qld-color-default-color-light-underline-visited-hover: #551a8b;
-          --qld-color-default-color-light-text-default: #333333;
-          --qld-color-default-color-light-text-lighter: #636363;
-          --qld-color-default-color-light-text-heading: #003549;
-          --qld-color-default-color-light-site-title: #022A50;
-          --qld-color-default-color-light-crest-fill: #022A50;
-          --qld-color-default-color-dark-background-default: #046994;
-          --qld-color-default-color-dark-background-default-shade: #005C84;
-          --qld-color-default-color-dark-background-alt: #080707;
-          --qld-color-default-color-dark-background-alt-shade: #161616;
-          --qld-color-default-color-dark-border-default: #4A93B3;
-          --qld-color-default-color-dark-border-alt: #09ACFE;
-          --qld-color-default-color-dark-action-primary: #4A93B3;
-          --qld-color-default-color-dark-action-primary-hover: #FFFFFF;
-          --qld-color-default-color-dark-action-secondary: #FFD559;
+          ${cssVariables}
         }
         </style>
         <main>
           ${Story()}
         </main>
         <div>
-          ${new Banner(breadcrumbs).html}
-          ${new Footer(footerData).html}
+          ${new Banner(bannerDataWithOverride).html}
+          ${new Footer(footerDataWithOverride).html}
         </div>
       `;
     },
