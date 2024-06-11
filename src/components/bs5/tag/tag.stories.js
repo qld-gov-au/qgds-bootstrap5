eaaa41e758e5c600bc90cc2e9b92ac36cd914c27
Elvi Shu <elvi.shu@smartservice.qld.gov.au>
2024-06-12 08:58:29 +1000

    [QOLOE-172]Add Status tag into Storybook, add a story to show all possible Status tag variants.

--- src/components/bs5/tag/tag.stories.js
+++ src/components/bs5/tag/tag.stories.js
@@ -2,6 +2,31 @@
 import { Tag } from './Tag.js';
 import defaultdata from './tag.data.json';
 
+const themeVariants = {}
+
+const sizes = {
+  "tag-small": "Small",
+  "tag-big": "Big (Default)",
+};
+
+const emphasis = {
+  "tag-low": "Low",
+  "tag-high": "High",
+};
+
+const types = {
+  "tag-neutral": "Neutral",
+  "tag-success": "Success",
+  "tag-warning": "Warning",
+  "tag-error": "Error",
+  "tag-information": "Information",
+}
+
+const iconPositions = {
+  "tag-icon-leading": "Leading icon",
+  "tag-icon-trailing": "Trailing icon",
+};
+
 export default {
   tags: ['autodocs'],
   title: 'Components/Tag',
@@ -36,6 +61,91 @@ export const Information = {
 // Action Tag story
 export const Action = {
   args: defaultdata.action,
+};
+
+// Status Tag story
+export const Status = {
+  args: defaultdata.status,
+  argTypes: {
+    size: {
+      description: "Size",
+      control: {
+        type: "radio",
+        labels: sizes,
+      },
+      options: Object.keys(sizes),
+    },
+    emphasis: {
+      description: "Emphasis",
+      control: {
+        type: "radio",
+        labels: emphasis,
+      },
+      options: Object.keys(emphasis),
+    },
+    iconSVG: {
+      description: "SVG content of the icon",
+      control: "text",
+    },
+    iconPosition: {
+      description: "Position of the icon placement",
+      control: {
+        type: "radio",
+        labels: iconPositions,
+      },
+      options: Object.keys(iconPositions),
+    },
+  },
+};
+
+/**
+ * Show status tags for all possible variants with various sizes, emphasis levels, and types.
+ * In the Default Light mode.
+ * This Story can be used to help in troubleshooting.
+ */
+export const AllStatusTagsInDefaultMode = {
+  render:() => {
+
+    //Map through the emphasis levels and sizes objects.
+    return Object.entries(emphasis).map(([emClass, emLabel]) => {
+
+      return Object.entries(sizes).map(([sizeClass, sizeLabel]) => {
+
+        //Construct tagItems for each types.
+        let tagItems = [];
+        Object.entries(types).map(([typeClass, typeLabel]) => {
+          tagItems.push({
+            content: `${typeLabel} ${sizeLabel} ${emLabel}`,
+            classes: `${typeClass}`,
+          });
+        });
+
+        //Generate Tag component markup from all possible tag types.
+        const tagHtml =  new Tag({
+          variant: defaultdata.status.variant,
+          tagItems: tagItems,
+          size: sizeClass,
+          emphasis: emClass,
+          iconSVG: defaultdata.status.iconSVG,
+          iconPosition: defaultdata.status.iconPosition,
+        }).html;
+
+        //Return Tag component markup in grid with tag's emphasis and size as label. 
+        return `<div class="d-grid mb-4">
+                  <div class="fw-bold">${emLabel} ${sizeLabel}</div>
+                  <div class="btn-toolbar">
+                    ${tagHtml}
+                  </div>
+                </div>`;
+
+      }).join('')
+    }).join('');
+  },
+  parameters: {
+    controls: {
+      disable: true,
+    },
+  },
 };
 
 // Filter Tag story
