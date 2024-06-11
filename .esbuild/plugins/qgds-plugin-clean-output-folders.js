6e364167f1dcec8628725b510f9e34eec2ea01a3
Elvi Shu <elvi.shu@smartservice.qld.gov.au>
2024-06-11 11:46:07 +1000

    [QOLOE-259]Set initialBuild flag to false.

--- .esbuild/plugins/qgds-plugin-clean-output-folders.js
+++ .esbuild/plugins/qgds-plugin-clean-output-folders.js
@@ -34,6 +34,7 @@ export default function cleanFoldersPlugin() {
           //   fs.rmSync(docsdir, { recursive: true });
           // }
         }
+        isInitialBuild = false;
       });
     },
   };
