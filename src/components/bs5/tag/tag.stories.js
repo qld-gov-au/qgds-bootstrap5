99d7f9545e9512cd5e43e788ee877774bcb5eba6
Elvi Shu <elvi.shu@smartservice.qld.gov.au>
2024-06-12 10:38:47 +1000

    [QOLOE-172]Update Status tags styling and move Storybook markup to separate function.

--- src/components/bs5/tag/tag.stories.js
+++ src/components/bs5/tag/tag.stories.js
@@ -27,6 +27,44 @@ const iconPositions = {
   "tag-icon-trailing": "Trailing icon",
 };
 
+/**
+ * Construct Status tag for all possible variants with various sizes, emphasis levels, and types.
+ * @returns {HTML} HTMLMarkup of the tags.
+ */
+function statusVariantsMarkup() {
+  //Map through the emphasis levels and sizes objects.
+  return Object.entries(emphasis).map(([emClass, emLabel]) => {
+    return Object.entries(sizes).map(([sizeClass, sizeLabel]) => {
+      //Construct tagItems for each types.
+      let tagItems = [];
+      Object.entries(types).map(([typeClass, typeLabel]) => {
+        tagItems.push({
+          content: `${typeLabel}`,
+          classes: `${typeClass}`,
+        });
+      });
+
+      //Generate Tag component markup from all possible tag types.
+      const tagHtml =  new Tag({
+        variant: defaultdata.status.variant,
+        tagItems: tagItems,
+        size: sizeClass,
+        emphasis: emClass,
+        iconSVG: defaultdata.status.iconSVG,
+        iconPosition: defaultdata.status.iconPosition,
+      }).html;
+
+      //Return Tag component markup in grid with tag's emphasis and size as label. 
+      return `<div class="d-grid mb-4">
+                <div class="fw-bold">${emLabel} ${sizeLabel}</div>
+                <div class="btn-toolbar">
+                  ${tagHtml}
+                </div>
+              </div>`;
+    }).join('')
+  }).join('');
+}
+
 export default {
   tags: ['autodocs'],
   title: 'Components/Tag',
@@ -61,6 +99,21 @@ export const Information = {
 // Action Tag story
 export const Action = {
   args: defaultdata.action,
+};
+
+// Filter Tag story
+export const Filter = {
+  args: defaultdata.filter,
+};
+
+// Large Tag story
+export const Large = {
+  args: defaultdata.large,
+};
+
+// Dark Tag story
+export const Dark = {
+  args: defaultdata.dark,
 };
 
 // Status Tag story
@@ -99,66 +152,16 @@ export const Status = {
 };
 
 /**
- * Show status tags for all possible variants with various sizes, emphasis levels, and types.
- * In the Default Light mode.
+ * Show the Default mode of Status tags in all possible variants. 
  * This Story can be used to help in troubleshooting.
  */
-export const AllStatusTagsInDefaultMode = {
+export const AllStatusVariantsInDefaultMode = {
   render:() => {
-
-    //Map through the emphasis levels and sizes objects.
-    return Object.entries(emphasis).map(([emClass, emLabel]) => {
-
-      return Object.entries(sizes).map(([sizeClass, sizeLabel]) => {
-
-        //Construct tagItems for each types.
-        let tagItems = [];
-        Object.entries(types).map(([typeClass, typeLabel]) => {
-          tagItems.push({
-            content: `${typeLabel} ${sizeLabel} ${emLabel}`,
-            classes: `${typeClass}`,
-          });
-        });
-
-        //Generate Tag component markup from all possible tag types.
-        const tagHtml =  new Tag({
-          variant: defaultdata.status.variant,
-          tagItems: tagItems,
-          size: sizeClass,
-          emphasis: emClass,
-          iconSVG: defaultdata.status.iconSVG,
-          iconPosition: defaultdata.status.iconPosition,
-        }).html;
-
-        //Return Tag component markup in grid with tag's emphasis and size as label. 
-        return `<div class="d-grid mb-4">
-                  <div class="fw-bold">${emLabel} ${sizeLabel}</div>
-                  <div class="btn-toolbar">
-                    ${tagHtml}
-                  </div>
-                </div>`;
-
-      }).join('')
-    }).join('');
+    return statusVariantsMarkup();
   },
   parameters: {
     controls: {
       disable: true,
     },
   },
-};
-
-// Filter Tag story
-export const Filter = {
-  args: defaultdata.filter,
-};
-
-// Large Tag story
-export const Large = {
-  args: defaultdata.large,
-};
-
-// Dark Tag story
-export const Dark = {
-  args: defaultdata.dark,
 };
