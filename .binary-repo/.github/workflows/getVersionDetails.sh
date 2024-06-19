fcf240065d684d5095ccfa95e593fdb2cebd46c9
William Dutton <william.dutton@qld.gov.au>
2024-06-19 14:15:13 +1000

    QOLOE-125 CICD setup for CDN deployment via Release Binary Repo

---
+++ .binary-repo/.github/workflows/getVersionDetails.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+REF="${GITHUB_BASE_REF:-${GITHUB_REF}}"
+TAG_VALUE=${GITHUB_REF/refs\/tags\//}
+
+#If on a tag, use the tagged version as source of truth.
+if [[ "$GITHUB_REF" == "${GITHUB_REF/refs\/tags\//}"  ]]; then
+  echo "Is not a tag"
+  version=v$(node -pe 'require("./package.json").version')
+  echo "version=${version}" >> $GITHUB_ENV
+else
+#Second best case := package version.
+  version=${TAG_VALUE}
+  echo "version=${version}" >> $GITHUB_ENV
+fi
+
+IFS='.' read -r MAJOR MINOR PATCH <<< "$version"
+echo "url='https://$1.qgov.net.au/./qgds-core/${MAJOR}/${version}'" >> $GITHUB_OUTPUT
