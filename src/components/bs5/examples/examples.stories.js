6d7c7ff88004d7707834c005d71da2f2721a7b8d
Mitch Anderson <mitch-anderson@outlook.com>
2024-06-21 10:32:19 +1000

    WIP: Kitchen sink as seperate story

---
+++ src/components/bs5/examples/examples.stories.js
@@ -0,0 +1,36 @@
+import { Header } from '../header/Header';
+import { Navbar } from '../navbar/Navbar';
+import { SearchInput } from "../searchInput/SearchInput";
+import { Footer } from "../footer/Footer";
+
+// Mock data
+import { header, search, navigation, footer } from './example.data.json';
+
+export default {
+  title: 'Examples/Template',
+  render: (args) => {
+    const headerHtml = new Header({ ...args, searchInput: new SearchInput(search).html }).html;
+    const navbarHtml = new Navbar(navigation).html;
+    const footerHtml = new Footer(footer).html;
+    return `${headerHtml}${navbarHtml}${footerHtml}`;
+  },
+  parameters: {
+    layout: 'fullscreen',
+  },
+};
+
+export const masterbranded_example = {
+  args: {
+    ...header,
+    ...navigation,
+    ...footer,
+  },
+  parameters: {
+    backgrounds: {
+      default: 'Light',
+      values: [
+        { name: 'Light', value: 'var(--qld-light-background)' },
+      ],
+    },
+  },
+};
