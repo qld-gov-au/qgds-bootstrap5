b19f062c4d2592c47a5f99ac2da98c36fd1823dd
Mitch Anderson <mitch-anderson@outlook.com>
2024-07-02 13:10:30 +1000

    WIP: Example for TMR

--- src/components/bs5/datatables/datatable.js
+++ src/components/bs5/datatables/datatable.js
@@ -1,17 +1,25 @@
 import Component from '../../../js/QGDSComponent.js';
 import template from "../table/table.hbs?raw";
-import $ from 'jquery';
-import 'datatables.net';
-import 'datatables.net-bs5';
+
+// Dynamically load jQuery and DataTables
+const loadDependencies = () => {
+  return Promise.all([
+    import('jquery').then(module => module.default),
+    import('datatables.net'),
+    import('datatables.net-bs5')
+  ]);
+};
 
 export class DataTable {
   constructor(data = {}) {
     const component = new Component(template, data);
     this.html = component.html;
 
-    // Initialize DataTables
-    $(document).ready(function() {
-      $('.table').DataTable();
+    // Initialize DataTables after the component is inserted into the DOM
+    loadDependencies().then(([jQuery]) => {
+      jQuery(document).ready(function() {
+        jQuery('.table').DataTable();
+      });
     });
   }
 }
