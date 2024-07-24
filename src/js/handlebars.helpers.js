a6af304828ec6df2532c17ebc2edb615bfcde8dc
Elvi Shu <elvi.shu@smartservice.qld.gov.au>
2024-07-24 15:02:25 +1000

    [QOLOE-421]Accessibility: print out descriptive format for time duration (on video).

--- src/js/handlebars.helpers.js
+++ src/js/handlebars.helpers.js
@@ -102,6 +102,63 @@ export default function handlebarsHelpers(handlebars) {
     // Call the formatDate helper with the determined date and format
     return handlebars.helpers.formatDate(dateString, dateToFormat, format);
   });
+
+  /**
+   * Format time duration into a string.
+   *
+   * Supports two formats: "short" and "long".
+   * - Short format: Display the duration in simplified format of "HH:MM:SS". It is the default format.
+   * - Long format: Display the duration in descriptive format of "X hours Y minutes Z seconds".
+   *
+   * @param {Object} duration - Duration object with properties 'hours', 'minutes', and 'seconds'.
+   * @param {String} format - Option for format type 'short' or 'long'. If none provided, 'short' is the defaut value.
+   *
+   * @returns {String} Formatted duration
+   * Examples:
+   * - 03:00:00 - 3 hours
+   * - 03:15:00 - 3 hours 15 minutes
+   * - 01:30:45 - 1 hour 30 minutes 45 seconds
+   * - 02:00:45 - 2 hours 45 seconds
+   * - 07:12 - 7 minutes 12 seconds
+   * - 00:45 - 45 seconds
+   *
+   * Usage:
+   * {{formatDuration duration}}
+   * {{formatDuration duration long}}
+   */
+  handlebars.registerHelper('formatDuration', function(duration, format) {
+    if (!duration) {
+      return "";
+    }
+    const {hours = "", minutes = "", seconds = ""} = duration;
+    let durationString = "";
+    let parts = [];
+
+    // Long format: "X hours Y minutes Z seconds"
+    if (format === "long") {
+      if (hours > 0) {
+        parts.push(`${hours} hour${hours > 1 ? 's' : ''}`);
+      }
+      if (minutes > 0) {
+        parts.push(`${minutes} minute${minutes > 1 ? 's' : ''}`);
+      }
+      if (seconds > 0) {
+        parts.push(`${seconds} second${seconds > 1 ? 's' : ''}`);
+      }
+      durationString = parts.join(" ");
+
+    // Short format: "HH:MM:SS"
+    } else {
+      // Omitting hours when zero
+      if (hours > 0) {
+        parts.push(hours.toString().padStart(2, 0));
+      }
+      parts.push(minutes.toString().padStart(2, 0));
+      parts.push(seconds.toString().padStart(2, 0));
+      durationString = parts.join(":");
+    }
+    return durationString;
+  });
 }
 
 if(typeof(Handlebars) !== 'undefined') {
