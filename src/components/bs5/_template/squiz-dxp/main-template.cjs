a0f7c411253abecd0bbfcaa30b49db118cf06062
Elvi Shu <elvi.shu@smartservice.qld.gov.au>
2024-08-28 13:54:10 +1000

    WIP - component template

---
+++ src/components/bs5/_template/squiz-dxp/main-template.cjs
@@ -0,0 +1,9 @@
+/**
+ * [Component] component
+ * Common js module to support DXP Component Service.
+ */
+module.exports = async function (input, info) {
+    return `<div class="[component]">
+    </div>
+    `;
+};
