2bb5b7351e7f4cfa54bc981629d07de1fbf6210c
Sen Chung <SenChung@users.noreply.github.com>
2025-01-16 08:58:55 +1000

    QOLOE-797 back to top set default text

--- src/components/bs5/backToTop/backToTop.functions.js
+++ src/components/bs5/backToTop/backToTop.functions.js
@@ -1,34 +1,34 @@
 export function backToTop() {
-    // Get the Back To Top object:
-    // The offset is the number of pixels after which the button is displayed
-    // The dynamic attribute is used to determine if the buttons CSS uses Relitive or Fixed positioning
+  // Get the Back To Top object:
+  // The offset is the number of pixels after which the button is displayed
+  // The dynamic attribute is used to determine if the buttons CSS uses Relitive or Fixed positioning
 
-    let mybutton = document.getElementById("backToTop");
-    let btnIsDynamic = mybutton.getAttribute("data-dynamic");
+  let mybutton = document.getElementById("backToTop");
+  let btnIsDynamic = mybutton.getAttribute("data-dynamic");
     
-    mybutton.addEventListener("click", goToTop);
+  mybutton.addEventListener("click", goToTop);
 
-    if (btnIsDynamic == "true") {
+  if (btnIsDynamic == "true") {
+    mybutton.classList.remove("show");
+    window.onscroll = function() {scrollFunction()};
+  }
+
+  function scrollFunction() {
+    if (btnIsDynamic == "true") { 
+      let offset = mybutton.getAttribute("data-offset");
+      if (document.body.scrollTop > offset || document.documentElement.scrollTop > offset) {
+        mybutton.classList.add("show");
+        mybutton.setAttribute("aria-hidden", false);
+      } else {
         mybutton.classList.remove("show");
-        window.onscroll = function() {scrollFunction()};
+        mybutton.setAttribute("aria-hidden", true);
+      }
     }
+  }
 
-    function scrollFunction() {
-        if (btnIsDynamic == "true") { 
-            let offset = mybutton.getAttribute("data-offset");
-            if (document.body.scrollTop > offset || document.documentElement.scrollTop > offset) {
-                mybutton.classList.add("show");
-                mybutton.setAttribute("aria-hidden", false);
-            } else {
-                mybutton.classList.remove("show");
-                mybutton.setAttribute("aria-hidden", true);
-            }
-        }
-    }
-
-    function goToTop() {
-        document.body.scrollTop = 0; // For Safari
-        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-        event.preventDefault();
-    }
+  function goToTop() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    event.preventDefault();
+  }
 }
