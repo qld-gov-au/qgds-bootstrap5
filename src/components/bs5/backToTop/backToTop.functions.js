ff85d6b8382b5f74ff70b2e2f3c5bf47a63771bd
Sen Chung <SenChung@users.noreply.github.com>
2025-01-22 11:48:44 +1000

    fix: Back to top aligned to Figma requirement

--- src/components/bs5/backToTop/backToTop.functions.js
+++ src/components/bs5/backToTop/backToTop.functions.js
@@ -1,30 +1,7 @@
 export function backToTop() {
-  // Get the Back To Top object:
-  // The offset is the number of pixels after which the button is displayed
-  // The dynamic attribute is used to determine if the buttons CSS uses Relitive or Fixed positioning
 
   let mybutton = document.getElementById("backToTop");
-  let btnIsDynamic = mybutton.getAttribute("data-btn-dynamic");
-  
   mybutton.addEventListener("click", goToTop);
-
-  if (btnIsDynamic == "true") {
-    mybutton.classList.remove("show");
-    window.onscroll = function() {scrollFunction()};
-  }
-
-  function scrollFunction() {
-    if (btnIsDynamic == "true") { 
-      let offset = mybutton.getAttribute("data-offset");
-      if (document.body.scrollTop > offset || document.documentElement.scrollTop > offset) {
-        mybutton.classList.add("show");
-        mybutton.setAttribute("aria-hidden", false);
-      } else {
-        mybutton.classList.remove("show");
-        mybutton.setAttribute("aria-hidden", true);
-      }
-    }
-  }
 
   function goToTop() {
     document.body.scrollTop = 0; // For Safari
