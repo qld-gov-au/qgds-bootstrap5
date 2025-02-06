48a89bdfe4828f18f3393f03a91d9c14c3e8afb1
Sen Chung <SenChung@users.noreply.github.com>
2025-02-06 14:02:53 +1000

    init: Link List Columns

---
+++ src/components/bs5/linkColumns/linkColumns.stories.js
@@ -0,0 +1,67 @@
+// ComponentExample.stories.js
+/**
+ * @file linkColumns.stories.js
+ * @description Storybook configuration file for the link Columns component.
+ * @module linkColumns.stories
+ */
+
+import { LinkColumns } from "./linkColumns.js";
+import defaultdata from "./linkColumns.data.json";
+
+export default {
+  tags: ["autodocs"],
+  title: "2. Layout/Link Columns",
+  
+  render: (args) => new LinkColumns(args).html,
+  args: defaultdata,
+
+  /**
+   * Additional parameters for the story.
+   *
+   * @type {Object}
+   * @property {Object} design - Configuration for the design parameter.
+   * @property {string} design.name - Name of the design parameter.
+   * @property {string} design.type - Type of the design parameter.
+   * @property {string} design.url - URL of the design parameter.
+   */
+  parameters: {
+    design: {
+      name: "QGDS Figma Reference",
+      type: "figma",
+       url: "https://www.figma.com/design/qKsxl3ogIlBp7dafgxXuCA/QGDS-UI-kit?node-id=23167-395563&t=RyMlGjeuzgl8p5Gx-0",
+    },
+  },
+};
+
+/**
+ * Default Link Columns story
+ */
+export const Default = {};
+
+export const Light = {
+    args: {
+        ...defaultdata,
+        theme: "light",
+    },
+};
+
+export const Alternative = {
+    args: {
+        ...defaultdata,
+        theme: "alt",
+    },
+};
+
+export const Dark = {
+    args: {
+        ...defaultdata,
+        theme: "dark",
+    },
+};
+
+export const DarkAlternative = {
+    args: {
+        ...defaultdata,
+        theme: "dark-alt",
+    },
+};
