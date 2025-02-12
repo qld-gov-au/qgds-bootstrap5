1409645c9986e31a24c08d576d8dd370a37c09f3
Sen Chung <SenChung@users.noreply.github.com>
2025-02-12 16:52:54 +1000

    fix: js logic scope error

--- src/components/bs5/backToTop/backToTop.functions.js
+++ src/components/bs5/backToTop/backToTop.functions.js
@@ -1,18 +1,20 @@
 export function backToTop() {
   let backToTop = document.getElementById("backToTop");
-  let minHightValue = backToTop.dataset.minPageHeight;
-  let minScreenHight = parseInt(window.innerHeight * minHightValue);
-  let backToTopPos = backToTop.getBoundingClientRect().top;
   
-  if (backToTopPos <= minScreenHight) {
-    backToTop.classList.add('hide');
-  } else {
-    backToTop.classList.remove('hide');
-    backToTop.addEventListener("click", function(){
-      document.body.scrollTop = 0; // For Safari
-      document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-      event.preventDefault();
-    });
-  }  
-  
+  if (backToTop) {
+    let minHightValue = backToTop.getAttribute("data-min-page-height"); // .dataset.minPageHeight;
+    let minScreenHight = parseInt(window.innerHeight * minHightValue);
+    let backToTopPos = backToTop.getBoundingClientRect().top;
+
+    if (backToTopPos <= minScreenHight) {
+      backToTop.classList.add("hide");
+    } else {
+      backToTop.classList.remove("hide");
+      backToTop.addEventListener("click", function () {
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+        event.preventDefault();
+      });
+    }
+  }
 }
