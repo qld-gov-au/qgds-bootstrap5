cc55be930f50b6cdd3b465904112e9156fd0ddfe
Steve Pegg <126438309+stvp-qld@users.noreply.github.com>
2025-03-18 19:28:13 +1000

    Adds buttons and cards to banner template, updates backgrounds and palettes

--- src/components/bs5/banner/banner.stories.js
+++ src/components/bs5/banner/banner.stories.js
@@ -1,20 +1,25 @@
 // Banner.stories.js
-import { Banner } from "./Banner.js";
+import Handlebars from "handlebars";
+
+import template from "./banner.hbs?raw";
 import defaultdata from "./banner.data.json";
 
-//Breadcrumbs
-import { Breadcrumbs } from "../breadcrumbs/Breadcrumbs.js";
+//Import data objects required for the banner and any nested components
 import breadcrumbdata from "../breadcrumbs/breadcrumbs.data.json";
-
-//Import components required for the banner
-import { Button } from "../button/Button.js";
+import buttondata from "../button/button.data.json";
+import carddata from "../card/card.data.json";
 
 export default {
   tags: ["autodocs"],
   title: "3. Components/Banners",
-  render: (args) => new Banner(args).html,
+  render: (args) => {
+    //Adds breadcrumbs to all banner stories, if it isn't already defined or false
+    args.breadcrumbs = args.breadcrumbs || breadcrumbdata["default"];
+    return new Handlebars.compile(template)(args);
+  },
+
   argTypes: {
-    variantClass: {
+    classes: {
       name: "Variant Class",
       description: `Settable backgrounds for the banner component`,
       control: {
@@ -28,6 +33,7 @@ export default {
           "dark with-pattern": "Dark (with pattern)",
         },
       },
+
       options: [
         "light",
         "alt",
@@ -37,17 +43,20 @@ export default {
         "dark with-pattern",
       ],
     },
+
     image: {
       table: {
         disable: true,
       },
     },
+
     breadcrumbs: {
       table: {
         disable: true,
       },
     },
-    ctabuttons: {
+
+    buttons: {
       table: {
         disable: true,
       },
@@ -67,29 +76,25 @@ export default {
 /**
  * Default banner with breadcrumbs
  */
-const breadcrumbs = new Breadcrumbs(breadcrumbdata["default"]);
 
 export const Default = {
   args: {
     ...defaultdata,
     abstract: "",
     image: false,
-    ctabuttons: false,
-    breadcrumbs: breadcrumbs.html,
   },
 };
 
 /**
- * "No-banner banner
+ * "No-banner" banner
  */
 export const NoBanner = {
   args: {
     ...defaultdata,
+    classes: "dark-alt",
     title: false,
     abstract: false,
     image: false,
-    ctabuttons: false,
-    breadcrumbs: breadcrumbs.html,
   },
 };
 
@@ -100,8 +105,6 @@ export const WithAbstract = {
   args: {
     ...defaultdata,
     image: false,
-    ctabuttons: false,
-    breadcrumbs: breadcrumbs.html,
   },
 };
 
@@ -112,10 +115,8 @@ export const WithTexture = {
   args: {
     ...defaultdata,
     title: "A long page title that wraps onto another line",
-    variantClass: "dark with-texture",
+    classes: ["dark", "with-texture"],
     image: false,
-    ctabuttons: false,
-    breadcrumbs: breadcrumbs.html,
   },
 };
 
@@ -126,10 +127,8 @@ export const WithTextureAlt = {
   args: {
     ...defaultdata,
     title: "A long page title that wraps onto another line",
-    variantClass: "alt with-texture",
+    classes: ["alt", "with-texture"],
     image: false,
-    ctabuttons: false,
-    breadcrumbs: breadcrumbs.html,
   },
 };
 
@@ -139,8 +138,6 @@ export const WithTextureAlt = {
 export const WithFeatureImage = {
   args: {
     ...defaultdata,
-    ctabuttons: false,
-    breadcrumbs: breadcrumbs.html,
   },
 };
 
@@ -150,55 +147,80 @@ export const WithFeatureImage = {
 export const WithFeatureImageAngle = {
   args: {
     ...defaultdata,
-    ctabuttons: false,
     image: {
       ...defaultdata.image,
-      classes: "banner-image--angle",
+      classes: ["banner-image", "banner-image--angle"],
     },
-    breadcrumbs: breadcrumbs.html,
   },
 };
 
 /**
- * Banner with call to action
+ * Banner with call to action buttons
  */
 
-let primaryButton = new Button({
-  variantClass: "btn-primary",
-  islink: true,
-  isdisabled: false,
-  label: "Apply online",
-  href: "https://google.com",
-  target: "_blank",
-});
-
-let secondaryButton = new Button({
-  variantClass: "btn-secondary",
-  islink: true,
-  isdisabled: false,
-  label: "Get help",
-  href: "https://google.com",
-  target: "_blank",
-});
-
-export const WithCallToAction = {
-  title: "Call to action/Buttons",
+export const WithCallToActionButtons = {
+  title: "3. Components/Banners/Call to action/Buttons",
   args: {
     ...defaultdata,
-    breadcrumbs: breadcrumbs.html,
     title: "Register your vehicle or motorcycle",
     abstract:
       "You can use this service to get a quote for your vehicle or motorcycle registration.",
-    ctablocks: [
+    buttons: [
       {
-        content: `
-          <!-- Bootstrap button group -->
-          <div class="btn-group" role="group" aria-label="Available options">
-            ${primaryButton.html}
-            ${secondaryButton.html}
-          </div>
-        `,
+        ...buttondata,
+        iconClass: false,
+      },
+      {
+        ...buttondata,
+        classes: ["btn-secondary"],
+        variantClass: "btn-secondary",
       },
     ],
+  },
+};
+
+/**
+ * Banner with call to action cards
+ */
+
+export const WithCallToActionCards = {
+  title: "3. Components/Banners/Call to action/Buttons",
+  args: {
+    ...defaultdata,
+
+    title: "Register your vehicle or motorcycle",
+    abstract:
+      "You can use this service to get a quote for your vehicle or motorcycle registration.",
+    cards: [
+      {
+        title: "Banner link 1",
+        variantClass: "default",
+        action: "single",
+        link: "https://www.qld.gov.au",
+        arrow: true,
+      },
+      {
+        title: "Banner link 2 with wrapping text onto another line",
+        variantClass: "alt",
+        action: "single",
+        link: "https://www.qld.gov.au",
+        arrow: true,
+      },
+      {
+        title: "Banner link 2 with wrapping text onto another line",
+        variantClass: "dark",
+        action: "single",
+        link: "https://www.qld.gov.au",
+        arrow: true,
+      },
+      {
+        title: "Banner link 3",
+        variantClass: "dark-alt",
+        action: "single",
+        link: "https://www.qld.gov.au",
+        arrow: true,
+      },
+    ],
+    image: false,
   },
 };
