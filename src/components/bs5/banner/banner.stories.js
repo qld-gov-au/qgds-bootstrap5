f30fec52d163d7dad920199a8d3a970f86353510
Steve Pegg <126438309+stvp-qld@users.noreply.github.com>
2025-03-25 15:49:35 +1000

    Updates banner filenames; adds breakpoint CSS vars

--- src/components/bs5/banner/banner.stories.js
+++ src/components/bs5/banner/banner.stories.js
@@ -8,30 +8,6 @@ import defaultdata from "./banner.data.json";
 import breadcrumbdata from "../breadcrumbs/breadcrumbs.data.json";
 import buttondata from "../button/button.data.json";
 
-const carddata = [
-  {
-    title: "Banner link 1",
-    variantClass: "default",
-    action: "single",
-    link: "https://www.qld.gov.au",
-    arrow: true,
-  },
-  {
-    title: "Banner link 2 with wrapping text onto another line",
-    variantClass: "alt",
-    action: "single",
-    link: "https://www.qld.gov.au",
-    arrow: true,
-  },
-  {
-    title: "Banner link 3",
-    variantClass: "dark-alt",
-    action: "single",
-    link: "https://www.qld.gov.au",
-    arrow: true,
-  },
-];
-
 export default {
   tags: ["autodocs"],
   title: "3. Components/Banners",
@@ -41,7 +17,38 @@ export default {
     return new Handlebars.compile(template)(args);
   },
 
+  decorators: [
+    (Story, context) => {
+      const { args } = context; // Destructure args from context
+
+      console.log(args);
+
+      // Normalise "none" values
+      if (args.variantClass === "none") args.variantClass = "";
+      if (args.backgroundType === "none" || args.backgroundType === "") {
+        args.backgroundType = "";
+        args.image = false;
+      }
+
+      if (args.backgroundType.includes("with-bg-image")) {
+        const activeVariant = args.variantClass || "default";
+        args.image = {};
+        args.image.url = `assets/img/banner-desktop-${activeVariant}.jpg`;
+      }
+
+      // Return the updated story
+      return Story({ args: { ...args } });
+    },
+  ],
+
   argTypes: {
+    // Disable controls for multiple fields
+    callToAction: { table: { disable: true } },
+    //image: { table: { disable: true } },
+    breadcrumbs: { table: { disable: true } },
+    cards: { table: { disable: true } },
+    buttons: { table: { disable: true } },
+
     variantClass: {
       name: "Variant Class",
       description: `Settable backgrounds for the banner component`,
@@ -50,88 +57,14 @@ export default {
         labels: {
           none: "Default",
           light: "Light",
-          alt: "Light Alternative",
+          "light-alt": "Light Alternative",
           dark: "Dark",
           "dark-alt": "Dark Alternative",
         },
       },
-      options: ["none", "light", "alt", "dark", "dark-alt"],
+      options: ["none", "light", "light-alt", "dark", "dark-alt"],
     },
 
-    image: {
-      table: {
-        disable: true,
-      },
-    },
-
-    breadcrumbs: {
-      table: {
-        disable: true,
-      },
-    },
-
-    cards: {
-      table: {
-        disable: true,
-      },
-    },
-
-    buttons: {
-      table: {
-        disable: true,
-      },
-    },
-  },
-
-  parameters: {
-    docs: {
-      controls: {
-        hideNoControlsWarning: true,
-      },
-    },
-  },
-};
-
-/**
- * Default banner with breadcrumbs
- */
-
-export const Default = {
-  args: {
-    ...defaultdata,
-    abstract: "",
-    image: false,
-  },
-};
-
-/**
- * "No-banner" banner
- */
-export const NoBanner = {
-  args: {
-    ...defaultdata,
-    variantClass: "no-banner dark-alt",
-    title: false,
-    abstract: false,
-    image: false,
-  },
-};
-
-/**
- * Basic Banners
- */
-
-export const BannerBasic = {
-  args: {
-    ...defaultdata,
-    title: "A long page title that wraps onto another line",
-    image: false,
-    abstract:
-      "Renew your licence at a customer service centre, government office or police station.",
-    callToAction: "none",
-  },
-
-  argTypes: {
     backgroundType: {
       name: "Background Type",
       control: {
@@ -139,44 +72,10 @@ export const BannerBasic = {
         labels: {
           none: "None",
           "with-texture": "With Texture",
+          "with-bg-image": "With Background Image",
         },
       },
-      options: ["none", "with-texture"],
-    },
-    callToAction: {
-      table: {
-        disable: true,
-      },
-    },
-  },
-};
-
-/**
- * Banner with feature image
- */
-export const BannerAdvanced = {
-  args: {
-    ...defaultdata,
-    abstract:
-      "Renew your licence at a customer service centre, government office or police station.",
-    callToAction: "buttons",
-    cards: [],
-    buttons: [],
-  },
-
-  argTypes: {
-    backgroundType: {
-      name: "Background Type",
-      description: `Background options for the banner component`,
-      control: {
-        type: "radio",
-        labels: {
-          none: "None",
-          "with-image": "With Image",
-          "with-texture": "With Texture",
-        },
-      },
-      options: ["none", "with-image", "with-texture"],
+      options: ["none", "with-texture", "with-bg-image"],
     },
 
     "image.classes": {
@@ -191,69 +90,174 @@ export const BannerAdvanced = {
       },
       options: ["default", "align-grid", "align-right"],
     },
+  },
+};
 
-    callToAction: {
-      name: "Call to action",
-      description: `Settable call to action for the banner component`,
+/**
+ * Default banner with breadcrumbs
+ */
+
+export const Default = {
+  args: {
+    ...defaultdata,
+    title: "Heading 1",
+    abstract: "",
+    backgroundType: "with-bg-image",
+    image: false,
+  },
+
+  argTypes: {
+    //Remove default controls that aren't needed here
+    callToAction: { table: { disable: true } },
+    image: { table: { disable: true } },
+    "image.classes": { table: { disable: true } },
+  },
+};
+
+/**
+ * "No-banner" banner
+ */
+export const NoBanner = {
+  args: {
+    ...defaultdata,
+    variantClass: "no-banner dark-alt",
+    title: false,
+    abstract: false,
+    image: false,
+  },
+
+  argTypes: {
+    //Remove default controls that aren't needed here
+    callToAction: { table: { disable: true } },
+    image: { table: { disable: true } },
+    "image.classes": { table: { disable: true } },
+  },
+};
+
+/**
+ * Basic Banners
+ */
+
+export const BannerBasic = {
+  args: {
+    ...defaultdata,
+    title: "Heading 1",
+    image: false,
+    abstract:
+      "Renew your licence at a customer service centre, government office or police station.",
+    callToAction: "none",
+  },
+
+  argTypes: {
+    //Remove default controls that aren't needed here
+    callToAction: { table: { disable: true } },
+    image: { table: { disable: true } },
+    "image.classes": { table: { disable: true } },
+  },
+};
+
+/**
+ * With Call To Action Buttons
+ */
+export const BannerAdvancedButtons = {
+  name: "Banner Advanced (With Buttons)",
+  args: {
+    ...defaultdata,
+    abstract:
+      "Renew your licence at a customer service centre, government office or police station.",
+    callToAction: "buttons",
+    cards: [],
+    buttons: [
+      {
+        ...buttondata,
+        iconClass: false,
+      },
+      {
+        ...buttondata,
+        classes: ["btn-secondary"],
+        variantClass: "btn-secondary",
+      },
+    ],
+  },
+
+  argTypes: {
+    //Add an extra option to the backgroundType control
+    backgroundType: {
+      name: "Background Type",
       control: {
         type: "radio",
         labels: {
-          default: "None",
-          buttons: "Buttons",
-          cards: "Cards",
+          none: "None",
+          "with-image": "With Image",
+          "with-texture": "With Texture",
         },
       },
-      options: ["default", "buttons", "cards"],
+      options: ["none", "with-image", "with-texture"],
     },
   },
+};
 
-  decorators: [
-    (Story, context) => {
-      const { args } = context; // Destructure args from context
-      const { callToAction, variantClass, backgroundType } = args; // Destructure specific properties
+/**
+ * With Call To Action Cards
+ */
+export const BannerAdvancedCards = {
+  name: "Banner Advanced (With Cards)",
+  args: {
+    ...defaultdata,
+    abstract:
+      "Renew your licence at a customer service centre, government office or police station.",
+    callToAction: "cards",
+    cards: [
+      {
+        title: "Banner link 1",
+        variantClass: "default",
+        action: "single",
+        link: "https://www.qld.gov.au",
+        arrow: true,
+      },
+      {
+        title: "Banner link 2",
+        variantClass: "default",
+        action: "single",
+        link: "https://www.qld.gov.au",
+        arrow: true,
+      },
+      {
+        title: "Banner link 3",
+        variantClass: "default",
+        action: "single",
+        link: "https://www.qld.gov.au",
+        arrow: true,
+      },
+    ],
+  },
 
-      // Handle "cards" callToAction
-      if (callToAction === "cards") {
-        args.buttons = [];
-        args.cards = carddata;
-      }
-
-      // Handle "buttons" callToAction
-      if (callToAction === "buttons") {
-        args.cards = [];
-        args.buttons = [
-          {
-            ...buttondata,
-            iconClass: false,
-          },
-          {
-            ...buttondata,
-            classes: ["btn-secondary"],
-            variantClass: "btn-secondary",
-          },
-        ];
-      }
-
-      // Normalise "none" values
-      if (variantClass === "none") args.variantClass = "";
-      if (backgroundType === "none") args.backgroundType = "";
-      if (backgroundType === "none") args.image = false;
-
-      // Return the updated story
-      return Story({ args: { ...args } });
+  argTypes: {
+    //Add an extra option to the backgroundType control
+    backgroundType: {
+      name: "Background Type",
+      control: {
+        type: "radio",
+        labels: {
+          none: "None",
+          "with-image": "With Image",
+          "with-texture": "With Texture",
+        },
+      },
+      options: ["none", "with-image", "with-texture"],
     },
-  ],
+  },
 };
 
 /**
  * Banner with feature image (angled)
  */
-export const WithFeatureImageAngle = {
-  args: {
-    ...defaultdata,
-    image: {
-      ...defaultdata.image,
-      classes: ["image-angle"],
-    },
-  },
-};
+// export const WithFeatureImageAngle = {
+//   args: {
+//     ...defaultdata,
+//     image: {
+//       ...defaultdata.image,
+//       classes: ["image-angle"],
+//     },
+//   },
+// };
