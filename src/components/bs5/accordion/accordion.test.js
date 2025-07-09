4c4902d667d6a34d7a016aad0b73f6b3db75e0c9
stevelynagh <steve.lynagh@smartservice.qld.gov.au>
2025-07-09 14:15:08 +1000

    test improvements

--- src/components/bs5/accordion/accordion.test.js
+++ src/components/bs5/accordion/accordion.test.js
@@ -16,7 +16,10 @@ import mockData from "./accordion.data.json";
 
 describe("Accordion", () => {
   const AccordionComponent = new Accordion(mockData);
-  const dom = new JSDOM(`<!DOCTYPE html>${AccordionComponent.html}`);
+  const dom = new JSDOM(`<!DOCTYPE html>${AccordionComponent.html}`, {
+    runScripts: "dangerously",
+    resources: "usable",
+  });
   const d = dom.window.document;
 
   it("Renders as expected", () => {
@@ -25,7 +28,6 @@ describe("Accordion", () => {
 
   test("An expanded item collapses when clicked", async () => {
     const collapse = d.querySelector(".accordion-collapse.show");
-    // const item = d.querySelector(".accordion-item");
     const button = collapse.parentElement.querySelector(".accordion-button");
 
     button.click();
@@ -42,26 +44,25 @@ describe("Accordion", () => {
     expect(isOpen).toBe(true);
   });
 
-  // test("All items expand when open toggle button is clicked.", () =>
-  //   new Promise((done) => {
-  //     // Click the toggle all button
-  //     d.querySelector(".accordion-toggle-btn").click();
+  test("All items expand when open toggle button is clicked.", async () => {
+    // Click the toggle all button
+    d.querySelector(".accordion-toggle-btn").click();
 
-  //     // wait 100ms
-  //     setTimeout(() => {
-  //       const items = d.querySelectorAll(".accordion-item .accordion-collapse");
+    console.log();
+    const isAllOpen = await new Promise((resolve) => {
+      setTimeout(() => {
+        const items = d.querySelectorAll(".accordion-item .accordion-collapse");
 
-  //       // expect all items to have "show" class
-  //       expect(
-  //         // items is a NodeListOf<Element>, so convert to Array to run .every method
-  //         Array.from(items).every((item) => {
-  //           // item.classList is a DomTokenList, so convert to Array to run .includes method (JSDom not helping us here.)
-  //           const ret = Array.from(item.classList).includes("show");
-  //           return ret;
-  //         }),
-  //       ).toBe(true);
-
-  //       done();
-  //     }, 500);
-  //   }));
+        // items is a NodeListOf<Element>, so convert to Array to run .every method
+        const success = Array.from(items).every((item) => {
+          // item.classList is a DomTokenList, so convert to Array to run .includes method (JSDom not helping us here.)
+          return Array.from(item.classList).includes("show");
+        });
+        console.log(success);
+        resolve(success);
+      }, 500);
+    });
+    // expect all items to have "show" class
+    expect(isAllOpen).toBe(true);
+  });
 });
