d695426b870820435430c6196bcc5e9aca3da7ab
Teena Francis <Teena.Francis@smartservice.qld.gov.au>
2025-08-01 18:12:58 +1000

    reduced the css, used utility classes, created page layout

---
+++ src/components/bs5/pageLayout/pageLayout.stories.js
@@ -0,0 +1,56 @@
+import { PageLayout } from "./pageLayout.js";
+//Data
+import masterbrand_variant from "../header/header.variant.masterBrand.data.json";
+import menu_state from "../navbar/navbar.variant.menuState.data.json";
+import globalAlertData from "../globalAlert/globalAlert.data.json";
+import tableData from "../table/table.data.json";
+import backToTopData from "../backToTop/backToTop.data.json";
+import footerData from "../footer/footer.data.json";
+
+const defaultData = {
+  cdn: ".", //for storybook it's ., for normal usage "PROD"
+  title: "title goes here",
+  backToTop: backToTopData,
+  header: masterbrand_variant,
+  navbar: menu_state,
+  table: tableData,  
+  globalAlert: globalAlertData.critical,
+  footer: footerData,
+};
+
+export default {
+  title: "6. Templates/Page Layout",
+  render: (args) => {
+    return new PageLayout(args).html;
+  },
+  args: defaultData,
+  argTypes: {},
+  parameters: {
+    layout: "fullscreen",
+    docs: {
+      controls: {},
+    },
+  },
+};
+
+/**
+ * Default head metadata
+ *
+ */
+export const Default = {
+  args: {
+    backToTop: {
+      hide_back_to_top: false,
+      min_page_height: 0,
+      is_fixed: true,
+
+      directionalLink: {
+        id: "",
+        label: "Back to top",
+        href: "#",
+        target: "_self",
+        class: "back-to-top up my-2 ms-auto",
+      },
+    },
+  },
+};
