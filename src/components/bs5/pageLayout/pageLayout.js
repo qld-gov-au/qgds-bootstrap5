d7d431402fab70e48741b4881b395fe0655e3af2
Teena Francis <Teena.Francis@smartservice.qld.gov.au>
2025-08-04 16:45:43 +1000

    corrected the typo error, `z-index-10` is removed for fixed condition as `fixed-bottom` has `z-index` style

--- src/components/bs5/pageLayout/pageLayout.js
+++ src/components/bs5/pageLayout/pageLayout.js
@@ -2,7 +2,7 @@ import Component from "../../../js/QGDSComponent.js";
 import template from "./pageLayout.hbs?raw";
 
 /**
- * Creates a new instance of the Link Column class.
+ * Creates a new instance of the page layout class.
  * @param {Object} data - The data object used to render the template. If no data is provided, the components example data file (component.data.json) will be used as a fallback.
  * @returns {Object} - An object containing the Handlebars template, data, and rendered HTML of the component.
  *
@@ -11,7 +11,7 @@ import template from "./pageLayout.hbs?raw";
  */
 
 export class PageLayout {
-  // Use the global Component class to create a new instance of the LinkColumn component.
+  // Use the global Component class to create a new instance of the page layout component.
   // A data object, containing the Handlebars placeholder replacement strings, should be provided as an argument.
 
   constructor(data = {}) {
