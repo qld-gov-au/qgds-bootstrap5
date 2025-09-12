5d5a8b4241dc7cea4dd0f1c8dd04b62229c92d88
Teena Francis <Teena.Francis@smartservice.qld.gov.au>
2025-09-12 12:17:03 +1000

    Made mdx file more clean by removing docs data into a separate file

---
+++ src/components/bs5/accordion/docs.data.js
@@ -0,0 +1,33 @@
+const designResources = [
+  {
+    text: "Component library (Figma)",
+    url: "https://www.figma.com/file/qKsxl3ogIlBp7dafgxXuCA/QLD-GOV-DDS?type=design&node-id=6276-45691&mode=design&t=chjWFr1CLu6KQXzg-4",
+  },
+  {
+    text: "Master component file (Figma)",
+    url: "https://www.figma.com/file/qKsxl3ogIlBp7dafgxXuCA/QLD-GOV-DDS?type=design&node-id=5990-98109&mode=design&t=ORVHLonASEFQzPeD-0",
+  },
+  {
+    text: "Design System website",
+    url: "https://www.design-system.health.qld.gov.au/components/accordions",
+  },
+  {
+    text: "Bootstrap component",
+    url: "https://getbootstrap.com/docs/5.3/components/accordion/",
+  },
+];
+
+const usageInstructions = `
+  <h3>Usage Instructions</h3>
+  <div>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras consequat
+    gravida nulla et elementum. Vivamus ac nisl ac nunc mollis tincidunt.
+    Maecenas sagittis sit amet tellus accumsan sagittis. Duis a magna eu sem
+    dapibus auctor sit amet in eros. Ut consectetur augue vitae metus efficitur
+    aliquam. Donec at velit venenatis, vulputate ipsum id, dignissim nibh.
+    Pellentesque nisi mauris, sodales vitae blandit vel, semper eu mi. In vitae
+    lectus nec urna condimentum convallis.
+  </div>
+  `;
+
+export { designResources, usageInstructions };
