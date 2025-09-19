bd84f781d796d8bc99b2a2c952191a2bffbc883d
Steve Lynagh <steve.lynagh@smartservice.qld.gov.au>
2025-09-19 11:49:36 +1000

    fix: add padding to SearchInput Stories so focus ring is visible.

--- src/components/bs5/searchInput/searchInput.stories.js
+++ src/components/bs5/searchInput/searchInput.stories.js
@@ -6,7 +6,7 @@ export default {
   tags: ["autodocs"],
   title: "3. Components/Search Input",
   render: (args) => {
-    return `<form action="https://uat.forgov.qld.gov.au/search" class="site-search">${new SearchInput(args).html}</form>`;
+    return `<form action="https://uat.forgov.qld.gov.au/search" class="site-search p-3">${new SearchInput(args).html}</form>`;
   },
 
   argTypes: {},
