30538f9cc6be51408b3ffac627122e4c7b409bf5
Teena Francis <Teena.Francis@smartservice.qld.gov.au>
2025-09-22 13:16:35 +1000

    commented setting position in javascript based on pageYOffset

--- src/components/bs5/quickexit/quickexit.functions.js
+++ src/components/bs5/quickexit/quickexit.functions.js
@@ -3,12 +3,12 @@ export function positionQuickExit() {
   if (quickexitInstances.length > 0) {
     const el = document.getElementsByClassName("qld-quick-exit")[0];
     if (document.documentElement.clientWidth > 992) {
-      if (window.pageYOffset > 200) {
+      /* if (window.pageYOffset > 200) {
         el.setAttribute("style", "position: 'fixed', top: '0px'");
       }
       if (window.pageYOffset < 200) {
         el.setAttribute("style", "position: 'sticky', top: '0px'");
-      }
+      } */
     } else {
       el.setAttribute("style", "position: 'fixed', top: 'auto'");
     }
