1c4d744667bcd99351c6dfc5e1119c7288f92ef3
Teena Francis <Teena.Francis@smartservice.qld.gov.au>
2025-09-22 13:13:22 +1000

    formatting commit

--- src/components/bs5/quickexit/quickexit.functions.js
+++ src/components/bs5/quickexit/quickexit.functions.js
@@ -1,7 +1,7 @@
 export function positionQuickExit() {
-  let quickexitInstances = document.getElementsByClassName('qld-quick-exit');
+  let quickexitInstances = document.getElementsByClassName("qld-quick-exit");
   if (quickexitInstances.length > 0) {
-    const el = document.getElementsByClassName('qld-quick-exit')[0];
+    const el = document.getElementsByClassName("qld-quick-exit")[0];
     if (document.documentElement.clientWidth > 992) {
       if (window.pageYOffset > 200) {
         el.setAttribute("style", "position: 'fixed', top: '0px'");
@@ -16,34 +16,38 @@ export function positionQuickExit() {
 }
 
 export function initQuickexit() {
-  var quickExitElement = document.getElementsByClassName('qld-quick-exit');
-  var quickExitButton = document.querySelector('.qld-quick-exit-button');
+  var quickExitElement = document.getElementsByClassName("qld-quick-exit");
+  var quickExitButton = document.querySelector(".qld-quick-exit-button");
 
-  if (quickExitElement.length > 0 && typeof (quickExitButton) !== 'undefined' && quickExitButton != null) {
+  if (
+    quickExitElement.length > 0 &&
+    typeof quickExitButton !== "undefined" &&
+    quickExitButton != null
+  ) {
     onbtnClick();
     onKeyDown();
   }
 }
 /**
-* onbtnClick -> clicking quick exit button a page
-* @return {undefined}
-**/
+ * onbtnClick -> clicking quick exit button a page
+ * @return {undefined}
+ **/
 function onbtnClick() {
-  const escapeSite = 'https://www.google.com.au/';
-  var quickExitButton = document.querySelector('.qld-quick-exit-button');
+  const escapeSite = "https://www.google.com.au/";
+  var quickExitButton = document.querySelector(".qld-quick-exit-button");
   quickExitButton.onclick = function () {
     return quickExit(escapeSite);
-  }
+  };
 }
 
 /**
-* onKeyDown -> escape keydown event
-* @return {undefined}
-**/
+ * onKeyDown -> escape keydown event
+ * @return {undefined}
+ **/
 function onKeyDown() {
-  const escapeSite = 'https://www.google.com.au/';
+  const escapeSite = "https://www.google.com.au/";
   // add hotkey trigger
-  document.addEventListener('keydown', function (e) {
+  document.addEventListener("keydown", function (e) {
     if (e.keyCode === 27) {
       quickExit(escapeSite);
       if (e) {
@@ -58,18 +62,18 @@ function onKeyDown() {
 }
 
 /**
-* quickExit function redirects a user on click and Esc key down
-* @param {string} site - site to replace on initiating the 'quick exit' ('Esc' key or clicking 'Close this site' button) function
-* @return {undefined}
-**/
+ * quickExit function redirects a user on click and Esc key down
+ * @param {string} site - site to replace on initiating the 'quick exit' ('Esc' key or clicking 'Close this site' button) function
+ * @return {undefined}
+ **/
 function quickExit(site) {
   // then redirect to a non-sensitive site
-  window.open(site, '_blank');
+  window.open(site, "_blank");
   window.location.replace(site);
   // remove as much info from URL as possible
   if (window.history) {
     try {
-      window.history.replaceState({}, '', '/');
+      window.history.replaceState({}, "", "/");
     } catch (e) {
       e.printStackTrace();
     }
