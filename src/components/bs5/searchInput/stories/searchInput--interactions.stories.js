6d369610c2a150252988eb4313f5c004da74fbab
Steven Lynagh <steve.lynagh@smartservice.qld.gov.au>
2026-09-17 09:24:12 +1000

    chore: separate search input interactions stories

--- src/components/bs5/searchInput/stories/searchInput--interactions.stories.js
+++ src/components/bs5/searchInput/stories/searchInput--interactions.stories.js
@@ -1,195 +1,22 @@
 /** @import {Meta, StoryObj} from "@storybook/html-vite" */
 
-import { SearchInput } from "./SearchInput.js";
-import defaultdata from "./searchInput.data.json";
-import metadata from "./metadata.json";
+import defaultdata from "../searchInput.data.json";
 import { expect } from "storybook/test";
-import { waitFor } from "../../../js/testingutils.js";
+import { waitFor } from "../../../../js/testingutils.js";
+import searchMeta from "./searchInput.stories.js";
 
 /** @type Meta */
 export default {
-  tags: ["autodocs", "extended"],
-  title: "3. Components/Search Input",
-  render: (args) => {
-    return `<form action="https://uat.forgov.qld.gov.au/search" class="site-search p-3">${new SearchInput(args).html}</form>`;
-  },
-  argTypes: {
-    buttonLabel: {
-      description: "The label for the search button",
-      control: { type: "text" },
-    },
-    hasDefaultSuggestions: {
-      description:
-        "This manipulates defaultSuggestions to on focus, shows default suggestions below the search input",
-      control: { type: "boolean" },
-    },
-    hasDynamicSuggestions: {
-      description: "This produces dynamic suggestions as the user types",
-      control: { type: "boolean" },
-    },
-    "dynamicSuggestionsServiceLink.href": {
-      description: "URL for the dynamic suggestions Related Services link",
-      control: { type: "text" },
-    },
-  },
-  parameters: {
-    coderefs: {
-      metadata,
-      partialname: "searchInput", //{{> searchInput }}
-    },
-    docs: {
-      controls: {
-        include: [
-          "buttonLabel",
-          "hasDynamicSuggestions",
-          "hasDefaultSuggestions",
-          "dynamicSuggestionsServiceLink.href",
-        ],
-      },
-      story: { height: "800px" },
-    },
-  },
+  ...searchMeta,
+  tags: ["!autodocs"], // Do not generate an overview page for these stories.
+  title: "3. Components/Search Input/Interactions", // Must be a string literal - see https://github.com/storybookjs/storybook/blob/next/MIGRATION.md#string-literal-titles
 };
 
-export const Default = {
-  args: {
-    ...defaultdata,
-    showDefaultSuggestions: true,
-    inputID: "default-search",
-  },
-  name: "Default - Outline Variant",
-};
-
-/**
- * With `customClass: is-filled`
- */
-export const FilledVariant = {
-  args: { ...defaultdata, customClass: "is-filled", inputID: "filled-search" },
-};
-
-export const FullWidth = {
-  args: {
-    ...defaultdata,
-    customClass: "full-width",
-    inputID: "fullwidth-search",
-  },
-};
-
-/**
- * Dark themed Search Input
- *
- * Apply a class <code>.dark</code> to the parent container of the <code>.qld-search-input</code> element.
- */
-export const Dark = {
-  args: {
-    ...defaultdata,
-    inputID: "dark-search",
-  },
-  globals: { backgrounds: { value: "dark" } },
-  decorators: [
-    (Story) => {
-      return `
-      <div class="dark">
-          ${Story()}
-      </div>
-      `;
-    },
-  ],
-};
-
-/**
- * Dark themed Search Input
- *
- * Apply a class <code>.dark</code> to the parent container of the <code>.qld-search-input</code> element.
- */
-export const DarkFilled = {
-  args: {
-    ...defaultdata,
-    customClass: "is-filled",
-    inputID: "DarkFilledSearch",
-  },
-  globals: { backgrounds: { value: "dark" } },
-  decorators: [
-    (Story) => {
-      return `
-      <div class="dark">
-          ${Story()}
-      </div>
-      `;
-    },
-  ],
-};
-
-/**
- * Custom Search Submit Handler
- *
- * Listen for the custom event `qgds-search-submit` and handle the search submission in your own way.
- */
-export const CustomSubmitHandler = {
-  parameters: {
-    chromatic: { disableSnapshot: true },
-  },
-  args: {
-    ...defaultdata,
-    inputName: "q",
-    tags: {},
-    defaultSuggestions: false,
-    hasDefaultSuggestions: false,
-    hasDynamicSuggestions: false,
-    dynamicSuggestionsServiceLink: false,
-    inputID: "CustomSubmitHandlerSearch",
-  },
-  decorators: [
-    (Story) => {
-      return `
-      <div class="p-3">
-          ${Story()}
-
-          <div style="padding: 2rem;">
-            <p>This example demonstrates how to listen for the custom event <code>qld-search-submit</code> and handle the search submission in your own way.</p>
-            <p>Open the browser console to see the event payload logged when you submit the search form, and review the storybook Code References panel for code examples.</p>
-          </div>
-          
-          <script>
-
-            // In a frontend environment, listen for the custom event \`qld-search-submit\`
-            // ...and write your own search submission logic. For example:
-            
-            document.addEventListener('qld-search-submit', function(event) {
-              
-              // Prevent the default form submission behavior
-              event.preventDefault();
-
-              // Access the search query and other form data from the event detail
-              const payload = event.detail;
-              const params = new URLSearchParams(payload.formdata);
-              
-              // Write your own custom function or logic here.
-              // For demonstration purposes, we'll just log the payload to the console.
-              console.log('Search Submit Event payload:', payload);
-
-              // or submit the form normally
-              // payload.form.submit();
-
-              // or redirect to a custom target with query parameters
-              window.location.href = 'https://dev.data.qld.gov.au/dataset?' + params.toString();
-
-            });
-
-          </script>
-      </div>
-      `;
-    },
-  ],
-};
-
-// Interactions
 /**
  * Search dropdown appears on focus
  * @type StoryObj
  */
 export const DropdownOnFocus = {
-  tags: ["!autodocs"],
   parameters: {
     docs: {
       description: {
@@ -199,7 +26,6 @@ export const DropdownOnFocus = {
     },
   },
   args: {
-    ...defaultdata,
     inputID: "DropdownOnFocus",
   },
   play: async ({ canvas, canvasElement, userEvent, step }) => {
@@ -220,7 +46,6 @@ export const DropdownOnFocus = {
  * @type StoryObj
  */
 export const TabBack = {
-  tags: ["!autodocs"], // testing story only
   parameters: {
     chromatic: { disableSnapshot: true }, // do not need the snapshot - this is an interaction test.
   },
@@ -250,19 +75,17 @@ export const TabBack = {
  * @type StoryObj
  */
 export const DynamicSuggestions = {
-  tags: ["!autodocs"], // testing story only
   parameters: {
     chromatic: { disableSnapshot: false }, // get a snapshot of this
   },
   args: {
     ...DropdownOnFocus.args,
   },
-  play: async ({ canvasElement, userEvent, context, step }) => {
+  play: async ({ canvasElement, userEvent, step }) => {
     const inputElement = canvasElement.querySelector(".qld-search-input input");
     const suggestionsElement = canvasElement.querySelector(
       ".qld-search-input .suggestions",
     );
-    // await DropdownOnFocus.play(context);
     await step(
       "Suggestions should show dynamic suggestions when typing",
       async () => {
@@ -287,7 +110,6 @@ export const DynamicSuggestions = {
  * @type StoryObj
  */
 export const EscapeClearsValue = {
-  tags: ["!autodocs"], // testing story only
   parameters: {
     chromatic: { disableSnapshot: true }, // do not need the snapshot - this is an interaction test.
   },
@@ -323,7 +145,6 @@ export const EscapeClearsValue = {
  * @type StoryObj
  */
 export const EscapeThenTab = {
-  tags: ["!autodocs"], // testing story only
   parameters: {
     chromatic: { disableSnapshot: true }, // do not need the snapshot - this is an interaction test.
   },
@@ -372,14 +193,13 @@ export const EscapeThenTab = {
  * @type StoryObj
  */
 export const EscapeSuggestions = {
-  tags: ["!autodocs"], // testing story only
   parameters: {
     chromatic: { disableSnapshot: true }, // do not need the snapshot - this is an interaction test.
   },
   args: {
     ...DropdownOnFocus.args,
   },
-  play: async ({ canvasElement, userEvent, context, step, canvas }) => {
+  play: async ({ canvasElement, userEvent, context, step }) => {
     const inputElement = canvasElement.querySelector(".qld-search-input input");
     const suggestionsElement = canvasElement.querySelector(
       ".qld-search-input .suggestions",
@@ -410,7 +230,6 @@ export const EscapeSuggestions = {
  * @type StoryObj
  */
 export const ClickOutside = {
-  tags: ["!autodocs"], // testing story only
   parameters: {
     chromatic: { disableSnapshot: true }, // do not need the snapshot - this is an interaction test.
   },
@@ -431,5 +250,3 @@ export const ClickOutside = {
     });
   },
 };
-
-// Click outside disappears dropdown.
---
+++ src/components/bs5/searchInput/stories/searchInput.stories.js
@@ -0,0 +1,179 @@
+/** @import {Meta} from "@storybook/html-vite" */
+
+import { SearchInput } from "../SearchInput.js";
+import defaultdata from "../searchInput.data.json";
+import metadata from "../metadata.json";
+
+/** @type Meta */
+export default {
+  tags: ["autodocs", "extended"],
+  title: "3. Components/Search Input",
+  render: (args) => {
+    return `<form action="https://uat.forgov.qld.gov.au/search" class="site-search p-3">${new SearchInput(args).html}</form>`;
+  },
+  args: defaultdata,
+  argTypes: {
+    buttonLabel: {
+      description: "The label for the search button",
+      control: { type: "text" },
+    },
+    hasDefaultSuggestions: {
+      description:
+        "This manipulates defaultSuggestions to on focus, shows default suggestions below the search input",
+      control: { type: "boolean" },
+    },
+    hasDynamicSuggestions: {
+      description: "This produces dynamic suggestions as the user types",
+      control: { type: "boolean" },
+    },
+    "dynamicSuggestionsServiceLink.href": {
+      description: "URL for the dynamic suggestions Related Services link",
+      control: { type: "text" },
+    },
+  },
+  parameters: {
+    coderefs: {
+      metadata,
+      partialname: "searchInput", //{{> searchInput }}
+    },
+    docs: {
+      controls: {
+        include: [
+          "buttonLabel",
+          "hasDynamicSuggestions",
+          "hasDefaultSuggestions",
+          "dynamicSuggestionsServiceLink.href",
+        ],
+      },
+      story: { height: "800px" },
+    },
+  },
+};
+
+export const Default = {
+  args: {
+    showDefaultSuggestions: true,
+    inputID: "default-search",
+  },
+  name: "Default - Outline Variant",
+};
+
+/**
+ * With `customClass: is-filled`
+ */
+export const FilledVariant = {
+  args: { customClass: "is-filled", inputID: "filled-search" },
+};
+
+export const FullWidth = {
+  args: {
+    customClass: "full-width",
+    inputID: "fullwidth-search",
+  },
+};
+
+/**
+ * Dark themed Search Input
+ *
+ * Apply a class <code>.dark</code> to the parent container of the <code>.qld-search-input</code> element.
+ */
+export const Dark = {
+  args: {
+    ...defaultdata,
+    inputID: "dark-search",
+  },
+  globals: { backgrounds: { value: "dark" } },
+  decorators: [
+    (Story) => {
+      return `
+      <div class="dark">
+          ${Story()}
+      </div>
+      `;
+    },
+  ],
+};
+
+/**
+ * Dark themed Search Input
+ *
+ * Apply a class <code>.dark</code> to the parent container of the <code>.qld-search-input</code> element.
+ */
+export const DarkFilled = {
+  args: {
+    customClass: "is-filled",
+    inputID: "DarkFilledSearch",
+  },
+  globals: { backgrounds: { value: "dark" } },
+  decorators: [
+    (Story) => {
+      return `
+      <div class="dark">
+          ${Story()}
+      </div>
+      `;
+    },
+  ],
+};
+
+/**
+ * Custom Search Submit Handler
+ *
+ * Listen for the custom event `qgds-search-submit` and handle the search submission in your own way.
+ */
+export const CustomSubmitHandler = {
+  parameters: {
+    chromatic: { disableSnapshot: true },
+  },
+  args: {
+    inputName: "q",
+    tags: {},
+    defaultSuggestions: false,
+    hasDefaultSuggestions: false,
+    hasDynamicSuggestions: false,
+    dynamicSuggestionsServiceLink: false,
+    inputID: "CustomSubmitHandlerSearch",
+  },
+  decorators: [
+    (Story) => {
+      return `
+      <div class="p-3">
+          ${Story()}
+
+          <div style="padding: 2rem;">
+            <p>This example demonstrates how to listen for the custom event <code>qld-search-submit</code> and handle the search submission in your own way.</p>
+            <p>Open the browser console to see the event payload logged when you submit the search form, and review the storybook Code References panel for code examples.</p>
+          </div>
+          
+          <script>
+
+            // In a frontend environment, listen for the custom event \`qld-search-submit\`
+            // ...and write your own search submission logic. For example:
+            
+            document.addEventListener('qld-search-submit', function(event) {
+              
+              // Prevent the default form submission behavior
+              event.preventDefault();
+
+              // Access the search query and other form data from the event detail
+              const payload = event.detail;
+              const params = new URLSearchParams(payload.formdata);
+              
+              // Write your own custom function or logic here.
+              // For demonstration purposes, we'll just log the payload to the console.
+              console.log('Search Submit Event payload:', payload);
+
+              // or submit the form normally
+              // payload.form.submit();
+
+              // or redirect to a custom target with query parameters
+              window.location.href = 'https://dev.data.qld.gov.au/dataset?' + params.toString();
+
+            });
+
+          </script>
+      </div>
+      `;
+    },
+  ],
+};
